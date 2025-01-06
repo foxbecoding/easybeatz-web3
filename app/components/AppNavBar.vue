637426444e3b9312903ba02b91ce170ed3ea4f04
foxbecoding <foxbecoding@gmail.com>
2025-01-06 17:32:16 -0600

    added component to nav

--- app/components/AppNavBar.vue
+++ app/components/AppNavBar.vue
@@ -12,10 +12,7 @@
       </label>
 
       <div class="flex-none">
-        <button class="btn btn-neutral text-xl mr-4">
-          <Icon icon="solar:wallet-2-bold" :ssr="true" class="text-2xl" />
-          Login
-        </button>
+        <AppWalletConnect />
         <button class="btn btn-neutral btn-square">
           <Icon icon="solar:cart-large-bold" class="text-2xl" />
         </button>
