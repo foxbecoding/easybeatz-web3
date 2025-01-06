d8322ca51a11102d48562436d8474a082c1f7a5b
foxbecoding <foxbecoding@gmail.com>
2025-01-06 17:32:05 -0600

    added connect button

--- app/components/AppWalletConnect.vue
+++ app/components/AppWalletConnect.vue
@@ -1,4 +1,9 @@
-<template></template>
+<template>
+  <button class="btn btn-neutral text-xl mr-4">
+    <Icon icon="solar:wallet-2-bold" :ssr="true" class="text-2xl" />
+    Login
+  </button>
+</template>
 
 <script setup lang="ts">
 
