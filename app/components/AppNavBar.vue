599930d24a5fcf7c7a9ac4e0c44f0e6e113e578a
foxbecoding <foxbecoding@gmail.com>
2025-01-06 15:12:45 -0600

    added nav bar

--- app/components/AppNavBar.vue
+++ app/components/AppNavBar.vue
@@ -1,21 +1,39 @@
 <template>
-  <div class="navbar bg-base-100">
-    <div class="flex-1">
-      <a class="btn text-xl">daisyUI</a>
-    </div>
-    <div class="flex-none">
-      <ul class="menu menu-horizontal px-1">
-        <li><a>Link</a></li>
-        <li>
-          <details>
-            <summary>Parent</summary>
-            <ul class="bg-base-100 rounded-t-none p-2">
-              <li><a>Link 1</a></li>
-              <li><a>Link 2</a></li>
-            </ul>
-          </details>
-        </li>
-      </ul>
+  <div class="navbar bg-base-100 relative">
+    <div class="mx-auto w-full max-w-[2560px] px-4 sm:px-8 2xl:px-16 flex items-center justify-between">
+
+      <div class="flex">
+        <a class="btn btn-neutral text-xl">LOGO</a>
+      </div>
+
+      <label class="input bg-neutral input-ghost flex items-center gap-2 max-w-[480px] w-full">
+        <input type="text" class="grow" placeholder="Search" />
+        <Icon icon="solar:magnifer-linear" width="24" height="24" />
+
+      </label>
+
+      <div class="flex-none">
+        <button class="btn btn-neutral text-xl mr-4">
+          <Icon icon="solar:wallet-2-bold" :ssr="true" class="text-2xl" />
+          Login
+        </button>
+        <button class="btn btn-neutral btn-square">
+          <Icon icon="solar:cart-large-bold" class="text-2xl" />
+        </button>
+      </div>
+
     </div>
   </div>
 </template>
+
+<style scoped>
+.navbar {
+  /*border-bottom: 1px solid #212121;*/
+}
+
+.searchbar-container {
+  max-width: 480px;
+  height: 48px;
+  padding: 4px;
+}
+</style>
