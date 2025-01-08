2c21b9afb39d2b18db4a58a9247e29ea4680b168
foxbecoding <foxbecoding@gmail.com>
2025-01-08 13:19:07 -0600

    implements class method logic

--- api-venv/api/users/views/login_nonce.py
+++ api-venv/api/users/views/login_nonce.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework import status
+from ..serializers import Web3LoginNonceSerializer
 
 class UserLoginNonceViewSet(viewsets.ViewSet):
     def get_permissions(self):
@@ -9,4 +10,11 @@ class UserLoginNonceViewSet(viewsets.ViewSet):
         return [permission() for permission in permission_classes]
 
     def create(self, request):
-        pass
+        serializer = Web3LoginNonceSerializer(data=request.data)
+        
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        nonce = serializer.save()
+
+        return Response({"data": nonce.nonce}, status=status.HTTP_201_CREATED)
