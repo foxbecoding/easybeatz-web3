4eb90e5efad5dee49422fd9811b24939f1172827
foxbecoding <foxbecoding@gmail.com>
2025-01-08 15:14:20 -0600

    added computed for isAuthenticated

--- app/store/user.ts
+++ app/store/user.ts
@@ -1,7 +1,16 @@
-export const userStore = defineStore("user-store", () => {
+export const useUserStore = defineStore("user-store", () => {
+  const accessKey = ref(null)
+  const pubkey = ref(null)
 
+  const isAuthenticated = computed(() => accessKey.value != null)
+
+  return {
+    accessKey,
+    isAuthenticated,
+    pubkey
+  }
 })
 
 if (import.meta.hot) {
-  import.meta.hot.accept(acceptHMRUpdate(userStore, import.meta.hot));
+  import.meta.hot.accept(acceptHMRUpdate(useUserStore, import.meta.hot));
 }
