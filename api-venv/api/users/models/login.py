f2af7169c8ff1c4fdec3420dabd9cc5b2970c2c0
foxbecoding <foxbecoding@gmail.com>
2025-01-08 10:47:43 -0600

    created UserLoginNonce model

--- api-venv/api/users/models/login.py
+++ api-venv/api/users/models/login.py
@@ -11,4 +11,3 @@ class UserLogin(models.Model):
     created = models.DateTimeField(auto_now_add=True, null=True)
     updated = models.DateTimeField(auto_now=True, null=True)
     deleted = models.DateTimeField(null=True)
-
