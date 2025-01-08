9af3d874a7a7ef6730bc8eddab0b0e6c396fec12
foxbecoding <foxbecoding@gmail.com>
2025-01-07 19:14:05 -0600

    updated appnavbar

--- app/components/AppNavBar.vue
+++ app/components/AppNavBar.vue
@@ -8,13 +8,13 @@
 
       <label class="input bg-neutral input-ghost flex items-center gap-2 max-w-[480px] w-full">
         <input type="text" class="grow" placeholder="Search" />
-        <Icon icon="solar:magnifer-linear" width="24" height="24" :ssr="true" />
+        <Icon icon="solar:magnifer-linear" width="24" height="24" />
       </label>
 
       <div class="flex-none">
         <AppWalletConnect />
         <button class="btn btn-neutral btn-square">
-          <Icon icon="solar:cart-large-bold" class="text-2xl" :ssr="true" />
+          <Icon icon="solar:cart-large-bold" class="text-2xl" />
         </button>
       </div>
 
