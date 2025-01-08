81cbf622a336f27dd1c21d2d0d7387fbde6c81c4
foxbecoding <foxbecoding@gmail.com>
2025-01-08 11:22:19 -0600

    updated urls

--- api-venv/api/users/urls.py
+++ api-venv/api/users/urls.py
@@ -4,4 +4,5 @@ from .views import *
 router = DefaultRouter()
 router.register(r"user", UserViewSet, basename="user")
 router.register(r"web3-login", UserLoginViewSet, basename="web3-login")
+router.register(r"web3-login-nonce", UserLoginNonceViewSet, basename="web3-login-nonce")
 urlpatterns = router.urls
