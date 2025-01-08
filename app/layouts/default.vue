15f2d777ed2f641e7f4bbcafa0845426a45ebfd7
foxbecoding <foxbecoding@gmail.com>
2025-01-08 17:44:20 -0600

    added sidenav

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,9 +1,23 @@
 <template>
-  <AppNavBar />
-  <div class="divider my-0"></div>
-  <main>
-    <p>Some default layout content shared across all pages</p>
-    <slot />
-  </main>
-  <AppFooter />
+  <div class="drawer lg:drawer-open">
+    <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content flex flex-col ">
+
+      <AppNavBar />
+      <div class="divider my-0"></div>
+      <main>
+        <p>Some default layout content shared across all pages</p>
+        <slot />
+      </main>
+
+    </div>
+    <div class="drawer-side">
+      <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
+      <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+        <!-- Sidebar content here -->
+        <li><a>Sidebar Item 1</a></li>
+        <li><a>Sidebar Item 2</a></li>
+      </ul>
+    </div>
+  </div>
 </template>
