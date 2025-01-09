656faf30df3208ee31cf9c9a0e3733c1097f7f9c
foxbecoding <foxbecoding@gmail.com>
2025-01-08 18:59:00 -0600

    added list content

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -6,17 +6,45 @@
       <AppNavBar />
       <div class="divider my-0"></div>
       <main>
-        <p>Some default layout content shared across all pages</p>
         <slot />
       </main>
 
     </div>
     <div class="drawer-side">
       <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
-      <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+      <ul class="menu bg-base-200 text-base-content min-h-full w-80 px-4 py-2">
         <!-- Sidebar content here -->
-        <li><a>Sidebar Item 1</a></li>
-        <li><a>Sidebar Item 2</a></li>
+        <img src="/logo.png" width="150px" class="px-4 pb-8" />
+        <li>
+          <a class="text-lg">
+            <Icon icon="solar:home-2-bold" width="24" height="24" />
+            Home
+          </a>
+        </li>
+        <li>
+          <a class="text-lg">
+            <Icon icon="solar:turntable-music-note-bold" width="24" height="24" />
+            Explore
+          </a>
+        </li>
+        <li>
+          <a class="text-lg">
+            <Icon icon="solar:music-library-2-bold" width="24" height="24" />
+            Library
+          </a>
+        </li>
+        <li>
+          <a class="text-lg">
+            <Icon icon="solar:upload-track-2-bold" width="24" height="24" />
+            Upload
+          </a>
+        </li>
+        <li>
+          <a class="text-lg">
+            <Icon icon="solar:playlist-2-bold" width="24" height="24" />
+            Playlist
+          </a>
+        </li>
       </ul>
     </div>
   </div>
