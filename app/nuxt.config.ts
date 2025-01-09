3e2da7020acd693181bc6d58483b4cefd4122782
foxbecoding <foxbecoding@gmail.com>
2025-01-09 11:44:45 -0600

    added env vars

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,6 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      COMPANY_NAME: process.env.COMPANY_NAME,
+      SITE_NAME: process.env.SITE_NAME,
+      SITE_DOMAIN: process.env.SITE_DOMAIN,
+      SITE_URL: process.env.SITE_URL,
+      API_WEB3_LOGIN: process.env.API_WEB3_LOGIN,
+      API_WEB3_LOGIN_NONCE: process.env.API_WEB3_LOGIN_NONCE
+    }
+  },
+
   devServer: {
     host: '0.0.0.0',  // Listen on all interfaces (necessary for Docker)
     port: 3000,
@@ -24,11 +35,6 @@ export default defineNuxtConfig({
     ]
   ],
 
-  //colorMode: {
-  //  preference: 'system', // default theme
-  //  dataValue: 'theme', // activate data-theme in <html> tag
-  //  classSuffix: '',
-  //},
   tailwindcss: { exposeConfig: true },
 
   compatibilityDate: '2025-01-03',
