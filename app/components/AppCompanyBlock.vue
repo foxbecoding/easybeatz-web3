73d75626c0b198c71800d06478f7d47475c02408
foxbecoding <foxbecoding@gmail.com>
2025-01-09 11:50:20 -0600

    added company block component

---
+++ app/components/AppCompanyBlock.vue
@@ -0,0 +1,39 @@
+<template>
+</template>
+
+<script setup lang="ts">
+
+interface LinkList {
+  title: string,
+  to: string
+}
+
+const config = useRuntimeConfig();
+const company_name = ref<string>(`${config.public.COMPANY_NAME}`)
+const auxLinks = ref<LinkList[]>([
+  { title: 'About', to: '/about' },
+  { title: 'Contact', to: '/contact' },
+  { title: 'FAQ', to: '/faq' },
+  // { title: 'How it works', to: '/how-it-works' },
+  // { title: 'Blog', to: '/blog' },
+  { title: 'Terms', to: '/terms' },
+  { title: 'Privacy', to: '/privacy' },
+])
+
+</script>
+<style scoped>
+.aux-links {
+  text-decoration: none;
+  font-size: 0.8rem;
+  opacity: 0.8;
+}
+
+.aux-links:hover {
+  opacity: 0.65;
+}
+
+.legal-content {
+  font-size: 0.8rem;
+  opacity: 0.8;
+}
+</style>
