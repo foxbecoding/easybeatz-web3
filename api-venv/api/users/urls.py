dc4e7a8d14b94ec1a16dd6778b9f73bff3adfbce
foxbecoding <foxbecoding@gmail.com>
2025-01-10 10:56:22 -0600

    removed refresh urls

--- api-venv/api/users/urls.py
+++ api-venv/api/users/urls.py
@@ -1,8 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from .views import *
 
-
-
 router = DefaultRouter()
 router.register(r"user", UserViewSet, basename="user")
 router.register(r"web3-login", UserLoginViewSet, basename="web3-login")
