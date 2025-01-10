5159901347fdd86e284ce88713fdf56714e7fb66
foxbecoding <foxbecoding@gmail.com>
2025-01-10 16:36:18 -0600

    implements method logic

--- api-venv/api/users/services/web3_login_service.py
+++ api-venv/api/users/services/web3_login_service.py
@@ -16,4 +16,18 @@ class Web3LoginService:
         self.signature = data["signature"]
 
     def run(self):
-       pass 
+       nonce = UserLoginNonce.objects.filter(pubkey=self.pubkey).last()
+
+        if not nonce:
+            return Response({"error": "No nonce found for this wallet address"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not self.verify_nonce(self.message, nonce.nonce):
+            return Response({"error": "Invalid message or nonce"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not self.verify_solana_signature(self.validated_data):
+            return Response({"error": "Verification failed"}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = self.find_or_create_user(self.pubkey)
+        access_token = self.authenticate_user(user)
+        
+        return Response({"access_token": access_token, "pubkey": user.pubkey, "username": user.username}, status=status.HTTP_200_OK)
