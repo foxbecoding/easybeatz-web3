74d4a3f3d3c0e77084e7ee99b9969f0d46d70c3a
foxbecoding <foxbecoding@gmail.com>
2025-01-13 19:30:44 -0600

    set base models for station

---
+++ api-venv/api/stations/models/__init__.py
@@ -0,0 +1,5 @@
+from .station import Station
+from .station_picture import StationPicture
+from .station_banner import StationBanner
+
+__all__ = ['Station', 'StationPicture', 'StationBanner']
--- api-venv/api/stations/models/station_picture.py
+++ api-venv/api/stations/models/station_picture.py
@@ -1,15 +1,13 @@
 from django.db import models
-from users.models import User
+from .station import Station
 
-class Station(models.Model):
-    user = models.OneToOneField(
-        User,
+class StationPicture(models.Model):
+    station = models.OneToOneField(
+        Station,
         on_delete=models.CASCADE,
         primary_key=True,
     )
-    name = models.CharField(max_length=90)
-    handle = models.CharField(max_length=60, unique=True)
-    description = models.TextField(null=True, blank=True)
+    file = models.FileField(upload_to='station/picture/')
     created = models.DateTimeField(auto_now_add=True, null=True)
     updated = models.DateTimeField(auto_now=True, null=True)
     deleted = models.DateTimeField(null=True)
