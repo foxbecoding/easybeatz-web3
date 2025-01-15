2b271d5de2b83c18b7d99f3f2ca1f1bbe3d52ee4
foxbecoding <foxbecoding@gmail.com>
2025-01-15 11:54:35 -0600

    added fields to station serializer

--- api-venv/api/stations/serializers/station_serializer.py
+++ api-venv/api/stations/serializers/station_serializer.py
@@ -2,6 +2,15 @@ from rest_framework import serializers
 from ..models import Station
 
 class StationSerializer(serializers.ModelSerializer):
+    is_owner = serializers.BooleanField(read_only=True) 
+    
     class Meta:
         model = Station
-        fields = ['name', 'handle', 'description']
+        fields = [
+            'name', 
+            'handle', 
+            'description', 
+            'email',
+            'is_owner',
+            'created'
+        ]
