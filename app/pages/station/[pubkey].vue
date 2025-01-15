8db3def812e2a844b04c3ee66aa4f18bb4d30780
foxbecoding <foxbecoding@gmail.com>
2025-01-15 11:33:52 -0600

    added on request method

--- app/pages/station/[pubkey].vue
+++ app/pages/station/[pubkey].vue
@@ -1,11 +1,27 @@
 <template>
-  <div>
-    {{ route }}
+  <div class="w-100 p-[0.5rem]">
+    <div class="mx-auto w-full max-w-[2560px] px-4 sm:px-8 2xl:px-16">
+      <img class="mask mask-squircle" src="/easy-glow.png" width="200" height="200" />
+      <p>@{{ route.params.pubkey }}</p>
+    </div>
   </div>
+
 </template>
 
 <script setup lang="ts">
+import { useAuthStore } from "@/store/auth";
 
 const route = useRoute();
-
+const config = useRuntimeConfig();
+const authStore = useAuthStore();
+const { status, data: station } = await useLazyFetch(`${config.public.API_STATION}/${route.params.pubkey}/public_station`, {
+  onRequest({ request, options }) {
+    if (authStore.accessToken) {
+      options.headers.set('Authorization', `Bearer ${authStore.accessToken}`)
+    }
+  },
+});
+watch(station, (newStation) => {
+  console.log("station: ", newStation)
+})
 </script>
