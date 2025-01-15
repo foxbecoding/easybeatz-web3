082032d8338886d8af1b0acd0a7609fffce250ab
foxbecoding <foxbecoding@gmail.com>
2025-01-14 23:31:28 -0600

    setup retrieve method for station

--- api-venv/api/stations/views/station.py
+++ api-venv/api/stations/views/station.py
@@ -1,13 +1,18 @@
 from rest_framework import viewsets
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from ..serializers import StationSerializer
 
 class StationViewSet(viewsets.ViewSet):
     def get_permissions(self):
-        permission_classes = [IsAuthenticated]
-        return [permission() for permission in permission_classes] 
+        permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
+
+    lookup_fields = ["pubkey"]
+
+    def retrieve(self, request, pubkey=None):
+        return Response(pubkey, status=status.HTTP_200_OK)
 
     def create(self, request):
         serializer = StationSerializer(data=request.data)
