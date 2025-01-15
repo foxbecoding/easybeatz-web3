ca972da832a53c761d655b4fd59b1044d8832e9c
foxbecoding <foxbecoding@gmail.com>
2025-01-15 11:34:04 -0600

    removed unused actions

--- api-venv/api/stations/views/station.py
+++ api-venv/api/stations/views/station.py
@@ -10,16 +10,8 @@ class StationViewSet(viewsets.ViewSet):
         permission_classes = [AllowAny]
         return [permission() for permission in permission_classes]
 
-    def retrieve(self, request, pk=None):
-        return Response(pk, status=status.HTTP_200_OK)
-
-    def create(self, request):
-        serializer = StationSerializer(data=request.data)
-        if not serializer.is_valid():
-            return Response({"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-        data = serializer.validated_data
-        return Response({"data": data}, status=status.HTTP_201_CREATED)
-
     @action(detail=True, methods=['get'])
     def public_station(self, request, pk=None):
+        is_owner = str(request.user) == str(pk)
+        print(is_owner)
         return Response(pk, status=status.HTTP_200_OK)
