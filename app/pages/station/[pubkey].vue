48b5543b33f66ef18fc8de7be272ef1ad51b970a
foxbecoding <foxbecoding@gmail.com>
2025-01-15 19:20:39 -0600

    added skeleton for station details

--- app/pages/station/[pubkey].vue
+++ app/pages/station/[pubkey].vue
@@ -1,10 +1,10 @@
 <template>
   <AppPageContainer>
-    <div class="flex">
+    <div v-if="station" class="flex">
       <div class="mr-4 min-w-[200px]">
         <NuxtImg class="mask mask-squircle" src='/easy-glow.png' width="200" height="200" />
       </div>
-      <div v-if="station">
+      <div>
         <p class="text-2xl font-semibold">{{ station.name ? station.name : 'Unnamed Station' }}</p>
         <p class="text-lg font-semibold">@{{ route.params.pubkey }}</p>
         <p class="mb-2 opacity-70">Joined {{ station.created }}</p>
@@ -12,7 +12,20 @@
         <button v-else class="btn btn-primary text-lg">Subscribe</button>
       </div>
     </div>
+    <div v-else class="flex">
+      <div class="mr-4 min-w-[200px]">
+        <div class="skeleton mask mask-squircle w-[200px] h-[200px]"></div>
+      </div>
+      <div class="flex flex-col w-[500px] gap-4">
+        <div class="skeleton h-4 w-full"></div>
+        <div class="skeleton h-4 w-full"></div>
+        <div class="skeleton h-4 w-48"></div>
+        <div class="skeleton h-[48px] w-[120px]"></div>
+      </div>
+    </div>
+
     <div class="divider mt-8"></div>
+
     <div class="grid grid-cols-6 md:grid-cols-6 sm:grid-cols-2 gap-8">
       <div v-for="i in demoAlbums" class="flex flex-col w-full gap-4">
         <div class="skeleton aspect-square w-full"></div>
