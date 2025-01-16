c78cb142fdec33d7fc11e02b0bede461de9cd534
foxbecoding <foxbecoding@gmail.com>
2025-01-16 01:05:14 -0600

    added station edit page

---
+++ app/pages/station/[id]/edit.vue
@@ -0,0 +1,12 @@
+<template>
+  <AppPageContainer>
+    EDIT STATION {{ pubkey }}
+  </AppPageContainer>
+</template>
+
+<script setup lang="ts">
+
+const route = useRoute();
+const pubkey = route.params.pubkey
+
+</script>
