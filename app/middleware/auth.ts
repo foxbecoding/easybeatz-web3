2831b2cc7cceed8d69720ace8ddb5e6c9ea35f69
foxbecoding <foxbecoding@gmail.com>
2025-01-16 16:41:45 -0600

    implements auth middleware logic

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,3 +1,15 @@
+import { useUserStore } from "@/store/user";
+import { useAuthStore } from "@/store/auth";
+
 export default defineNuxtRouteMiddleware((to, from) => {
-  console.log(to.name)
+  const authStore = useAuthStore();
+  const userStore = useUserStore();
+
+  if (!authStore.isAuthenticated) {
+    return navigateTo({ name: 'index' });
+  }
+
+  if (to.name == 'station-id-edit' && (userStore.pubkey != to.params.id)) {
+    return navigateTo({ name: 'index' });
+  }
 })
