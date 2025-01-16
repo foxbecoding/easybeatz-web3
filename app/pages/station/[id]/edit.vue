f1fc78526e5cbdc8e35b08db8a86af768ea598f9
foxbecoding <foxbecoding@gmail.com>
2025-01-16 17:11:10 -0600

    added title and form element

--- app/pages/station/[id]/edit.vue
+++ app/pages/station/[id]/edit.vue
@@ -1,15 +1,17 @@
 <template>
   <AppPageContainer>
-    EDIT STATION {{ pubkey }}
+    <h1 class="text-3xl font-bold">Customize Station</h1>
+    <form ref="form"></form>
   </AppPageContainer>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
   middleware: ["auth"]
-  // or middleware: 'auth'
 })
+
 const route = useRoute();
 const pubkey = route.params.pubkey
+const form = ref()
 
 </script>
