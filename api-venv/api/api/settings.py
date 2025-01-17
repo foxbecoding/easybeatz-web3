b159f8ff17c1c5c41687689e84fd91acd7f4f864
foxbecoding <foxbecoding@gmail.com>
2025-01-17 11:45:07 -0600

    updated api settings

--- api-venv/api/api/settings.py
+++ api-venv/api/api/settings.py
@@ -166,7 +166,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+
 STATIC_URL = 'static/'
+# STATIC_ROOT = BASE_DIR / 'static'
 
 # Static media files
 MEDIA_ROOT=os.getenv('MEDIA_ROOT')
