b7f220fa1b8c34f453bcc2c6a6b66b8e18487d26
foxbecoding <foxbecoding@gmail.com>
2025-01-17 13:05:23 -0600

    refactors middleware logic

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,9 +1,7 @@
-import { useUserStore } from "@/store/user";
 import { useAuthStore } from "@/store/auth";
 
 export default defineNuxtRouteMiddleware((to, from) => {
   const authStore = useAuthStore();
-  const userStore = useUserStore();
 
   if (!authStore.isAuthenticated) {
     return navigateTo({ name: 'index' });
