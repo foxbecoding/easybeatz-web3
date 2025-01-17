da48ed5a03d0b60b0e92c30bb4b1d59a92d3dc1b
foxbecoding <foxbecoding@gmail.com>
2025-01-17 12:24:23 -0600

    added new serialzer

--- api-venv/api/stations/serializers/__init__.py
+++ api-venv/api/stations/serializers/__init__.py
@@ -1,3 +1,4 @@
 from .station_serializer import StationSerializer
+from .public_station_serializer import PublicStationSerializer
 
-__all__ = ['StationSerializer']
+__all__ = ['StationSerializer', 'PublicStationSerializer']
