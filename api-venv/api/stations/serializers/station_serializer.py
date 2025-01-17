b6d9b970447788e3c4d2ec58cfa3d8d40d861922
foxbecoding <foxbecoding@gmail.com>
2025-01-16 22:29:35 -0600

    implements handle validation

--- api-venv/api/stations/serializers/station_serializer.py
+++ api-venv/api/stations/serializers/station_serializer.py
@@ -11,3 +11,10 @@ class StationSerializer(serializers.ModelSerializer):
             'email',
             'created'
         ]
+
+    def validate(self, attrs):
+
+        # check if handle is valid
+        if attrs['handle'] and not str(attrs['handle']).isalnum():
+            raise serializers.ValidationError({"handle": "Handle can only contain strings and numbers."})
+        return attrs
