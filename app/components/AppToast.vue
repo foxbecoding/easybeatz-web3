3926de27f6dc12493e5b9575e9524a847cb907ce
foxbecoding <foxbecoding@gmail.com>
2025-01-17 18:10:26 -0600

    implements toast component

---
+++ app/components/AppToast.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="toast toast-end">
+    <div class="alert alert-info">
+      <span>New mail arrived.</span>
+    </div>
+    <div class="alert alert-success">
+      <span>Message sent successfully.</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
