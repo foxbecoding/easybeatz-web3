b4867237e1d84314fcd23a4708ab2859c02fa3ca
foxbecoding <foxbecoding@gmail.com>
2025-01-17 20:06:15 -0600

    refactors serializer logic

--- api-venv/api/stations/serializers/station_serializer.py
+++ api-venv/api/stations/serializers/station_serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from ..models import Station
+from users.models import User
 
 class StationSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,8 +13,23 @@ class StationSerializer(serializers.ModelSerializer):
         ]
 
     def validate(self, attrs):
-
+        request = self.context['request']
+        
         # check if handle is valid
         if attrs['handle'] and not str(attrs['handle']).isalnum():
             raise serializers.ValidationError({"handle": "Handle can only contain strings and numbers."})
+
+
+        user = User.objects.filter(pubkey=request.user).first()
+
+        station_ins = Station(
+            user=user,
+            name=attrs['name'],
+            handle=attrs['handle'],
+            description=attrs['description'],
+            email=attrs['email'],
+        )
+
+        station_ins.save()
+
         return attrs
