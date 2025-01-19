0f6415794dac745f7dc7e4f9cec575e73bf15c8b
foxbecoding <foxbecoding@gmail.com>
2025-01-18 22:30:56 -0600

    removes unused serializer

--- api-venv/api/stations/serializers/__init__.py
+++ api-venv/api/stations/serializers/__init__.py
@@ -1,5 +1,4 @@
 from .station_serializer import StationSerializer
 from .public_station_serializer import PublicStationSerializer
-from .edit_station_serializer import EditStationSerializer
 
-__all__ = ['StationSerializer', 'PublicStationSerializer', 'EditStationSerializer']
+__all__ = ['StationSerializer', 'PublicStationSerializer']
