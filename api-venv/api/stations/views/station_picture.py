1082eb3fd3d96ca585af308bfe9c8f04614dc559
foxbecoding <foxbecoding@gmail.com>
2025-01-19 20:43:56 -0600

    implements view logic

--- api-venv/api/stations/views/station_picture.py
+++ api-venv/api/stations/views/station_picture.py
@@ -4,8 +4,9 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from ..serializers import  PublicStationSerializer
-from ..models import StationPicture
+from ..serializers import StationPictureSerializer
+from ..models import Station, StationPicture
+from users.models import User
 
 class StationPictureViewSet(viewsets.ViewSet):
     def get_permissions(self):
@@ -18,7 +19,19 @@ class StationPictureViewSet(viewsets.ViewSet):
     @action(detail=False, methods=['post'])
     def upload(self, request):
         # check if user has station
-        # get use station
-        # serialize data
-        # create or update
-        pass
+        user = User.objects.filter(pubkey=str(request.user)).first()
+        station = Station.objects.filter(pk=user.pk).first()
+
+        if not station:
+            return Response({"error": "Unauthorized"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        request.data['station'] = station
+        serializer = StationPictureSerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response({"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+        
+        serializer.save()
+
+        return Response("Picture uploaded successfully", status=status.HTTP_200_OK)
