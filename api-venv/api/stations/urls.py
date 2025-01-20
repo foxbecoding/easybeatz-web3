ce01fc73d4f1f8d860fc742037816bd729c2c94f
foxbecoding <foxbecoding@gmail.com>
2025-01-19 20:45:17 -0600

    included viewset in urls

--- api-venv/api/stations/urls.py
+++ api-venv/api/stations/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 router = DefaultRouter()
 router.register(r"station", StationViewSet, basename="station")
+router.register(r"station-picture", StationPictureViewSet, basename="station-picture")
 urlpatterns = router.urls
