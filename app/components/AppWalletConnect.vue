f50b80bb7c00536d665be55f86444835d7c13e55
foxbecoding <foxbecoding@gmail.com>
2025-01-21 16:51:15 -0600

    implements new styling

--- app/components/AppWalletConnect.vue
+++ app/components/AppWalletConnect.vue
@@ -1,5 +1,6 @@
 <template>
-  <button @click="!isConnecting ? connectWallet() : false" class="btn btn-neutral text-xl" :disabled="isConnecting">
+  <button @click="!isConnecting ? connectWallet() : false" class="btn btn-neutral rounded-[1rem] text-xl"
+    :disabled="isConnecting">
     <Icon class="text-2xl" icon="solar:wallet-2-bold" />
     Login
     <Icon icon="token-branded:phantom" class="text-2xl" />
