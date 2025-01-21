5f35efc2992664686931a8de9d654563221ca579
foxbecoding <foxbecoding@gmail.com>
2025-01-20 23:24:32 -0600

    removed validators

--- api-venv/api/stations/serializers/station_picture_serializer.py
+++ api-venv/api/stations/serializers/station_picture_serializer.py
@@ -12,26 +12,26 @@ class StationPictureSerializer(serializers.ModelSerializer):
         allowed_formats = ['png', 'jpeg', 'jpg', 'avif']
 
         # Check file size
-        max_size_mb = 6
-        if value.size > max_size_mb * 1024 * 1024:
-            raise serializers.ValidationError({"picture": f"Image file size must not exceed {max_size_mb}MB."})
+        # max_size_mb = 6
+        # if value.size > max_size_mb * 1024 * 1024:
+        #     raise serializers.ValidationError(f"Image file size must not exceed {max_size_mb}MB.")
 
         # Check image format
         try:
             image = Image.open(value)
             if str(image.format).lower() not in allowed_formats:
-                raise serializers.ValidationError({"picture": f"Unsupported image format. Allowed formats are: {', '.join(allowed_formats)}."})
+                raise serializers.ValidationError(f"Unsupported image format. Allowed formats are: {', '.join(allowed_formats)}.")
 
             # Check image dimensions
-            width, height = image.size
-            if width < 98 or height < 98:
-                raise serializers.ValidationError({"picture": "Image dimensions must be at least 98x98 pixels."})
-
-            # Ensure it's a square
-            if width != height:
-                raise serializers.ValidationError("Image must be a square (equal width and height).")
+            # width, height = image.size
+            # if width < 98 or height < 98:
+            #     raise serializers.ValidationError("Image dimensions must be at least 98x98 pixels.")
+            #
+            # # Ensure it's a square
+            # if width != height:
+            #     raise serializers.ValidationError("Image must be a square(1:1 ration)")
 
         except Exception as e:
-            raise serializers.ValidationError({"picture": "Invalid image file."}) 
+            raise serializers.ValidationError("Invalid image file.") 
 
         return value
