500274933fd92a9fcd6ca4ecb60cd4eca9ab0ea0
foxbecoding <foxbecoding@gmail.com>
2025-01-22 17:58:54 -0600

    adds models

--- api-venv/api/albums/models/__init__.py
+++ api-venv/api/albums/models/__init__.py
@@ -3,5 +3,8 @@ from .album_cover import AlbumCover
 from .track import Track
 from .track_mp3 import TrackMp3
 from .track_wav import TrackWav
+from .track_stem import TrackStem
+from .track_price import TrackPrice
+from .track_exclusive_price import TrackExclusivePrice
 
-__all__ = ['Album', 'AlbumCover' ,'Track', 'TrackMp3', 'TrackWav']
+__all__ = ['Album', 'AlbumCover' ,'Track', 'TrackMp3', 'TrackWav', 'TrackStem', 'TrackPrice', 'TrackExclusivePrice']
