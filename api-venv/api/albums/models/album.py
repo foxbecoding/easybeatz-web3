2680143713723c3498c52b2d11cdbb64b1401d63
foxbecoding <foxbecoding@gmail.com>
2025-01-21 23:45:26 -0600

    updates method logic

--- api-venv/api/albums/models/album.py
+++ api-venv/api/albums/models/album.py
@@ -11,7 +11,7 @@ class Album(models.Model):
     )
     genres = models.ManyToManyField(Genre, related_name='albums')
     title = models.CharField(max_length=120, default='')
-    aid = models.CharField(default='')
+    aid = models.CharField(default='', unique=TrueG
     slug = models.SlugField(blank=True, default='')
     bio = models.TextField()
     created = models.DateTimeField(auto_now_add=True, null=True)
