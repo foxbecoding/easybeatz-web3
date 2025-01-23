da093bdb879c385d43e9035e4d87f54adbe81e52
foxbecoding <foxbecoding@gmail.com>
2025-01-23 14:00:40 -0600

    fixes method error

--- api-venv/api/albums/views/track.py
+++ api-venv/api/albums/views/track.py
@@ -13,5 +13,5 @@ class TrackViewSet(viewsets.ViewSet):
         if self.action in needs_auth: permission_classes = [IsAuthenticated]
         return [permission() for permission in permission_classes]
 
-    def create(self, request): 
+    def create(self, request):
         pass
