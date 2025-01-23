369af4c63e345568edfdda573caaad6404a11cca
foxbecoding <foxbecoding@gmail.com>
2025-01-22 18:09:54 -0600

    adds models to admin

--- api-venv/api/albums/admin.py
+++ api-venv/api/albums/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Album, AlbumCover, Track, TrackMp3, TrackWav
+from .models import Album, AlbumCover, Track, TrackMp3, TrackWav, TrackStem, TrackPrice, TrackExclusivePrice
 
 # Register your models here.
 admin.site.register(Album)
@@ -7,4 +7,7 @@ admin.site.register(AlbumCover)
 admin.site.register(Track)
 admin.site.register(TrackMp3)
 admin.site.register(TrackWav)
+admin.site.register(TrackStem)
+admin.site.register(TrackPrice)
+admin.site.register(TrackExclusivePrice)
 
