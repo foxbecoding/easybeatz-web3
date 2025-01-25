bf6602fcb4df737bdd592cc97ca0792a7b71046d
foxbecoding <foxbecoding@gmail.com>
2025-01-24 19:27:56 -0600

    adds new mode

--- api-venv/api/albums/models/__init__.py
+++ api-venv/api/albums/models/__init__.py
@@ -5,6 +5,7 @@ from .track_mp3 import TrackMp3
 from .track_wav import TrackWav
 from .track_stem import TrackStem
 from .track_price import TrackPrice
+from .track_collaborator import TrackCollaborator
 from .track_exclusive_price import TrackExclusivePrice
 
-__all__ = ['Album', 'AlbumCover' ,'Track', 'TrackMp3', 'TrackWav', 'TrackStem', 'TrackPrice', 'TrackExclusivePrice']
+__all__ = ['Album', 'AlbumCover' ,'Track', 'TrackMp3', 'TrackWav', 'TrackStem', 'TrackPrice', 'TrackExclusivePrice', 'TrackCollaborator']
