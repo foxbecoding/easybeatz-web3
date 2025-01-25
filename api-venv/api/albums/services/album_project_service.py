68e3dbc8c93b0105e878a6e0626ef7ad69781450
foxbecoding <foxbecoding@gmail.com>
2025-01-25 15:00:05 -0600

    modifies service logic

--- api-venv/api/albums/services/album_project_service.py
+++ api-venv/api/albums/services/album_project_service.py
@@ -11,18 +11,20 @@ class AlbumProjectService:
         self.album = {}
         self.album_cover = {}
         self.tracks = []
+        self.__set_form_data()
 
-    def set_data(self):
+
+    def __set_form_data(self):
         self.__set_album_data() 
         print(self.album)
 
-
-    def validate_form_data(self):
-        pass
 
     def __set_album_data(self):
         self.album = {
             "title": self.form_data.get(f'album[title]'),
             "bio": self.form_data.get(f'album[bio]'),
         }
+
+    def is_form_data_valid(self) -> bool:
+        return False 
 
