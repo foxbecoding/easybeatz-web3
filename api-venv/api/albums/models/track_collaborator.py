c60b26f80fe704b32fe5fec502183f347b7429a5
foxbecoding <foxbecoding@gmail.com>
2025-01-24 19:23:35 -0600

    implements new model

---
+++ api-venv/api/albums/models/track_collaborator.py
@@ -0,0 +1,15 @@
+from django.db import models
+from .track import Track
+from stations.models import Station
+
+class TrackCollaborator(models.Model):
+    track = models.ForeignKey(
+        Track,
+        on_delete=models.CASCADE,
+        related_name='collaborators'
+    )
+    station = models.OneToOneField(Station, on_delete=models.SET_NULL, blank=True, null=True, related_name='collaborations')
+    pubkey = models.CharField(max_length=100, default='')
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now=True, null=True)
+    deleted = models.DateTimeField(null=True)
