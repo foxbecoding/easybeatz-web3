4e3856406ba364d4a00f1d62bde515861985b5f0
foxbecoding <foxbecoding@gmail.com>
2025-01-27 12:39:03 -0600

    includes track form serializer

--- api-venv/api/albums/serializers/__init__.py
+++ api-venv/api/albums/serializers/__init__.py
@@ -1,3 +1,4 @@
 from .album_form_data_serializer import AlbumFormSerializer
+from .track_form_data_serializer import TrackFormSerializer
 
-__all__ = ['AlbumFormSerializer']
+__all__ = ['AlbumFormSerializer', 'TrackFormSerializer']
