7b410c3537a88f79d04b1b49b6f754b86db6bfbc
foxbecoding <foxbecoding@gmail.com>
2025-01-28 20:40:03 -0600

    adds serializers

--- api-venv/api/albums/serializers/__init__.py
+++ api-venv/api/albums/serializers/__init__.py
@@ -1,4 +1,25 @@
 from .album_form_data_serializer import AlbumFormSerializer
 from .track_form_data_serializer import TrackFormSerializer
+from .create_album_serializer import CreateAlbumSerializer
+from .create_album_cover_serializer import CreateAlbumCoverSerializer
+from .create_track_serializer import CreateTrackSerializer
+from .create_track_mp3_serializer import CreateTrackMp3Serializer
+from .create_track_wav_serializer import CreateTrackWavSerializer
+from .create_track_stem_serializer import CreateTrackStemSerializer
+from .create_track_collaborator_serializer import CreateTrackCollaboratorSerializer
+from .create_track_price_serializer import CreateTrackPriceSerializer
+from .create_track_exclusive_price_serializer import CreateTrackExclusivePriceSerializer
 
-__all__ = ['AlbumFormSerializer', 'TrackFormSerializer']
+__all__ = [
+    'AlbumFormSerializer',
+    'TrackFormSerializer',
+    'CreateAlbumSerializer',
+    'CreateAlbumCoverSerializer',
+    'CreateTrackSerializer',
+    'CreateTrackMp3Serializer',
+    'CreateTrackWavSerializer',
+    'CreateTrackStemSerializer',
+    'CreateTrackCollaboratorSerializer',
+    'CreateTrackPriceSerializer',
+    'CreateTrackExclusivePriceSerializer',
+]
