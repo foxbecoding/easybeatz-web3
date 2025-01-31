7df92c3797b115cb91bf965e422c19038e70e924
foxbecoding <foxbecoding@gmail.com>
2025-01-31 14:29:40 -0600

    implements container setup

---
+++ app/pages/station/project/components/CreateProjectContainer.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { type Genre } from "@/services/models/genre";
+import { type Mood } from "@/services/models/mood";
+
+const props = defineProps<{
+  genres: Genre[];
+  moods: Mood[];
+}>();
+</script>
+
+<template></template>
