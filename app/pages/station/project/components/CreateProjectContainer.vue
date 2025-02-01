cf5d413a7ee715f59cc8d0c65083083cbb103b59
foxbecoding <foxbecoding@gmail.com>
2025-01-31 20:57:20 -0600

    adds card for more structured styling

--- app/pages/station/project/components/CreateProjectContainer.vue
+++ app/pages/station/project/components/CreateProjectContainer.vue
@@ -12,10 +12,17 @@ const { albumForm, isStep1, isStep2, isStep3, isStep2Active } = useCreateProject
 </script>
 
 <template>
-  <ul class="steps w-full">
-    <li class="step" :class="isStep1 ? 'step-primary' : ''">Project details</li>
-    <li class="step" :class="isStep2Active ? 'step-primary' : ''">Tracks</li>
-    <li class="step" :class="isStep3 ? 'step-primary' : ''">Review</li>
-  </ul>
-  <AlbumForm v-show="!isStep2 || !isStep3" />
+  <div class="card bg-neutral max-w-[1200px] w-full shadow-md block mx-auto">
+    <div class="card-body">
+      <ul class="steps w-full">
+        <li class="step" :class="isStep1 ? 'step-primary' : ''">Project details</li>
+        <li class="step" :class="isStep2Active ? 'step-primary' : ''">Tracks</li>
+        <li class="step" :class="isStep3 ? 'step-primary' : ''">Review</li>
+      </ul>
+      <div class="py-4">
+        <AlbumForm v-if="!isStep2 || !isStep3" />
+      </div>
+    </div>
+  </div>
+
 </template>
