eb8ac94183ca143eed4905acfa17de5f031f2a63
foxbecoding <foxbecoding@gmail.com>
2025-01-31 20:56:58 -0600

    implements base structure for album cover form

--- app/pages/station/project/components/AlbumForm.vue
+++ app/pages/station/project/components/AlbumForm.vue
@@ -1,5 +1,20 @@
 <script setup lang="ts">
 
+const albumCoverForm = {
+  label: "Add project cover",
+  text: "*Recommended: For best quality, provide a square image that is at least 200x200 pixels."
+}
+
 </script>
 
-<template></template>
+<template>
+
+  <h4 class="text-2xl font-bold md:mb-4">{{ albumCoverForm.label }}</h4>
+  <p class="mb-4 max-w-[400px] block md:hidden"> {{ albumCoverForm.text }} </p>
+  <div class="flex">
+    <button class="btn btn-base-100 btn-square h-[120px] w-[120px]">
+      <Icon icon="solar:camera-add-bold" class="text-4xl" />
+    </button>
+    <p class="ml-4 max-w-[300px] hidden md:block"> {{ albumCoverForm.text }} </p>
+  </div>
+</template>
