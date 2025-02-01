9864b9a810c3aa01273847fb25202c95912356eb
foxbecoding <foxbecoding@gmail.com>
2025-01-31 18:51:09 -0600

    implements composable

--- app/pages/station/project/components/CreateProjectContainer.vue
+++ app/pages/station/project/components/CreateProjectContainer.vue
@@ -2,6 +2,8 @@
 import { type Genre } from "@/services/models/genre";
 import { type Mood } from "@/services/models/mood";
 
+const { isStep1, isStep2, isStep3 } = useCreateProject();
+
 const props = defineProps<{
   genres: Genre[] | null;
   moods: Mood[] | null;
@@ -10,8 +12,8 @@ const props = defineProps<{
 
 <template>
   <ul class="steps w-full">
-    <li class="step step-primary">Project details</li>
-    <li class="step step-primary">Tracks</li>
-    <li class="step">Review</li>
+    <li class="step" :class="isStep1 ? 'step-primary' : ''">Project details</li>
+    <li class="step" :class="isStep2 ? 'step-primary' : ''">Tracks</li>
+    <li class=" step" :class="isStep3 ? 'step-primary' : ''">Review</li>
   </ul>
 </template>
