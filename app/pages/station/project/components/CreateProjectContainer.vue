2ba3cdb29a531794de595a3d3340aac856fe4760
foxbecoding <foxbecoding@gmail.com>
2025-01-31 19:51:35 -0600

    implements AlbumForm component

--- app/pages/station/project/components/CreateProjectContainer.vue
+++ app/pages/station/project/components/CreateProjectContainer.vue
@@ -1,19 +1,21 @@
 <script setup lang="ts">
 import { type Genre } from "@/services/models/genre";
 import { type Mood } from "@/services/models/mood";
-
-const { isStep1, isStep2, isStep3 } = useCreateProject();
+import AlbumForm from "./AlbumForm.vue"
 
 const props = defineProps<{
   genres: Genre[] | null;
   moods: Mood[] | null;
 }>();
+
+const { albumForm, isStep1, isStep2, isStep3, isStep2Active } = useCreateProject();
 </script>
 
 <template>
   <ul class="steps w-full">
     <li class="step" :class="isStep1 ? 'step-primary' : ''">Project details</li>
-    <li class="step" :class="isStep2 ? 'step-primary' : ''">Tracks</li>
-    <li class=" step" :class="isStep3 ? 'step-primary' : ''">Review</li>
+    <li class="step" :class="isStep2Active ? 'step-primary' : ''">Tracks</li>
+    <li class="step" :class="isStep3 ? 'step-primary' : ''">Review</li>
   </ul>
+  <AlbumForm v-show="!isStep2 || !isStep3" />
 </template>
