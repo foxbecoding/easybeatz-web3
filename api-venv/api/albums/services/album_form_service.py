3bec298aed9320afc8a75e462127e97191167a87
foxbecoding <foxbecoding@gmail.com>
2025-02-03 17:52:45 -0600

    fixes class errors

--- api-venv/api/albums/services/album_form_service.py
+++ api-venv/api/albums/services/album_form_service.py
@@ -3,7 +3,7 @@ from .album_project_service import AlbumProjectService
 class AlbumFormService(AlbumProjectService):
     def __init__(self, request_data) -> None:
         super().__init__(request_data)
-        self._set_album_form_data()
+        super()._set_album_form_data()
 
     def is_valid(self) -> bool:
         return super()._is_album_form_data_valid()
