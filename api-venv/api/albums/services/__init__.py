c146e376976412467ee4e0dc150f6abe71a66ba4
foxbecoding <foxbecoding@gmail.com>
2025-02-03 14:20:43 -0600

    adds service class

--- api-venv/api/albums/services/__init__.py
+++ api-venv/api/albums/services/__init__.py
@@ -1,3 +1,4 @@
 from .album_project_service import AlbumProjectService
+from .album_form_service import AlbumFormService
 
-__all__ = ['AlbumProjectService']
+__all__ = ['AlbumProjectService', 'AlbumFormService']
