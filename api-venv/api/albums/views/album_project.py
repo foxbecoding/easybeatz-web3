3da2586785077fffdaf2808755d1a191affe8320
foxbecoding <foxbecoding@gmail.com>
2025-02-03 14:44:10 -0600

    implements view class action

--- api-venv/api/albums/views/album_project.py
+++ api-venv/api/albums/views/album_project.py
@@ -2,7 +2,8 @@ from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
-from ..services import AlbumProjectService
+from rest_framework.decorators import action
+from ..services import AlbumProjectService, AlbumFormService
 from users.models import User
 from stations.models import Station
 
@@ -25,3 +26,10 @@ class AlbumProjectViewSet(viewsets.ViewSet):
             return Response({"errors": service.errors})
         service.save(station)
         return Response("Easy Beatz", status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['post'])
+    def validate_album_form(self, request):
+        service = AlbumFormService(request.data)
+        if not service.is_valid():
+            return Response({"errors": service.errors})
+        return Response(None, status=status.HTTP_200_OK)
