febb0a62c55899817b1a175760fe263ca6e5c107
foxbecoding <foxbecoding@gmail.com>
2025-02-03 15:02:29 -0600

    renames method

--- app/services/models/album.ts
+++ app/services/models/album.ts
@@ -1,6 +1,6 @@
 import { type ApiData } from "@/composables/useApi";
 
-export const validateAlbumForm = async (data: any) => {
+export const albumFormValidator = async (data: any) => {
     const config = useRuntimeConfig();
     const fetchPath = `${config.public.API_ALBUM_PROJECT}/validate_album_form/`;
     const apiData: ApiData = { method: 'POST', path: fetchPath, data, isMultiPart: true };
