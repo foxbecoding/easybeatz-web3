f2b54cf4551b0cc441965bf6de57fedbedda6821
foxbecoding <foxbecoding@gmail.com>
2025-02-04 13:25:12 -0600

    implements component base logic

--- app/pages/station/project/components/TrackForm.vue
+++ app/pages/station/project/components/TrackForm.vue
@@ -1,5 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useCreateProjectStore } from "@/store/createProject";
+
+const createProjectStore = useCreateProjectStore();
+
+const nextStepHandler = async () => {
+  createProjectStore.next();
+}
+
+const backStepHandler = async () => {
+  createProjectStore.back();
+}
+</script>
 
 <template>
   <h1 class="text-2xl">Track Form</h1>
+  <div class="flex justify-between">
+    <button @click="backStepHandler()" class="btn text-lg rounded-[1rem]">
+      <Icon icon="solar:alt-arrow-left-line-duotone" class="text-2xl" />
+      Back
+    </button>
+    <button @click="nextStepHandler()" class="btn btn-secondary text-lg rounded-[1rem]">
+      Next
+      <Icon icon="solar:alt-arrow-right-line-duotone" class="text-2xl" />
+    </button>
+  </div>
 </template>
