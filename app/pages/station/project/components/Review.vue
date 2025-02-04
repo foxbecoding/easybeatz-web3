92e4362832651515945feffb714cf471e4e07cb7
foxbecoding <foxbecoding@gmail.com>
2025-02-04 13:24:54 -0600

    implements Review component

---
+++ app/pages/station/project/components/Review.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useCreateProjectStore } from "@/store/createProject";
+
+const createProjectStore = useCreateProjectStore();
+
+const backStepHandler = async () => {
+  createProjectStore.back();
+}
+</script>
+
+<template>
+  <h1 class="text-2xl">Review</h1>
+  <div class="flex justify-between">
+    <button @click="backStepHandler()" class="btn text-lg rounded-[1rem]">
+      <Icon icon="solar:alt-arrow-left-line-duotone" class="text-2xl" />
+      Back
+    </button>
+    <button class="btn btn-primary text-lg rounded-[1rem]">
+      Submit
+    </button>
+  </div>
+</template>
