e3d2f97336172fb6f06e4fc38dfd2f4ebed49fc7
foxbecoding <foxbecoding@gmail.com>
2025-02-03 18:15:34 -0600

    removes unused props

--- app/pages/station/project/components/CreateProjectContainer.vue
+++ app/pages/station/project/components/CreateProjectContainer.vue
@@ -9,20 +9,24 @@ const props = defineProps<{
   moods: Mood[] | null;
 }>();
 
-const { albumForm, isStep1, isStep2, isStep3, isStep2Active } = useCreateProject();
+const { albumForm, activeStep } = useCreateProject();
+
+const test = computed(() => activeStep.value);
 </script>
 
 <template>
   <div class="card bg-neutral max-w-[1200px] w-full shadow-md block mx-auto">
     <div class="card-body">
+      {{ albumForm }}
       <ul class="steps w-full">
-        <li class="step" :class="isStep1 ? 'step-primary' : ''">Project details</li>
-        <li class="step" :class="isStep2Active ? 'step-primary' : ''">Tracks</li>
-        <li class="step" :class="isStep3 ? 'step-primary' : ''">Review</li>
+        <li class="step" :class="activeStep === 'step1' || activeStep === 'step2' ? 'step-primary' : ''">Project details
+        </li>
+        <li class="step" :class="activeStep === 'step2' ? 'step-primary' : ''">Tracks</li>
+        <li class="step" :class="activeStep === 'step3' ? 'step-primary' : ''">Review</li>
       </ul>
       <div class="pt-4">
-        <AlbumForm v-if="!isStep2 || !isStep3" />
-        <TrackForm v-else-if="isStep2 && !isStep3" />
+        <AlbumForm v-if="activeStep === 'step1'" />
+        <TrackForm v-if="activeStep === 'step2'" />
       </div>
     </div>
   </div>
