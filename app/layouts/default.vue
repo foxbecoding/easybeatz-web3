e9cdc73d2c010bddb0cb03c060ca6aecea97b414
foxbecoding <foxbecoding@gmail.com>
2025-02-05 14:29:56 -0600

    fixes layout issues

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -11,19 +11,22 @@
       </main>
 
     </div>
-    <div class="lg:drawer-side min-h-full bg-base-200 hidden">
+    <div class="lg:drawer-side z-40 bg-base-200 hidden">
       <label for="app-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
-      <NuxtLink :to="{ name: 'index' }"><img src="/logo.png" width="200px" class="px-4 py-4" /></NuxtLink>
-      <ul class="menu bg-base-200 text-base-content w-80 px-4 py-2">
-        <li v-for="(item, i) in menuList" :key="i">
-          <NuxtLink class="text-lg rounded-[1rem] font-semibold" :to="{ name: item.name }" active-class="bg-neutral">
-            <Icon :icon="item.icon" class="text-2xl mr-2" :class="activeLinkIcon(item.name)" />
-            {{ item.label }}
-          </NuxtLink>
-        </li>
-        <div class="divider"></div>
-      </ul>
-      <AppCompanyBlock />
+      <aside class="bg-base-200 w-80">
+        <NuxtLink :to="{ name: 'index' }"><img src="/logo.png" width="200px" class="px-4 py-4" /></NuxtLink>
+        <ul class="menu bg-base-200 text-base-content w-80 px-4 py-2">
+          <li v-for="(item, i) in menuList" :key="i">
+            <NuxtLink class="text-lg rounded-[1rem] font-semibold" :to="{ name: item.name }" active-class="bg-neutral">
+              <Icon :icon="item.icon" class="text-2xl mr-2" :class="activeLinkIcon(item.name)" />
+              {{ item.label }}
+            </NuxtLink>
+          </li>
+          <div class="divider"></div>
+        </ul>
+        <AppCompanyBlock />
+
+      </aside>
     </div>
   </div>
 </template>
