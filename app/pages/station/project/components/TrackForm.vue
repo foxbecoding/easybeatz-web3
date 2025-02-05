c251c88ea4301a25b88a34adbf6ad6b659f3d11e
foxbecoding <foxbecoding@gmail.com>
2025-02-05 16:35:52 -0600

    renames store for clarity

--- app/pages/station/project/components/TrackForm.vue
+++ app/pages/station/project/components/TrackForm.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { useCreateProjectStore } from "@/store/createProject";
 
-const createProjectStore = useCreateProjectStore();
+const projectStore = useCreateProjectStore();
 
-const sanitizeInput = (index: any, event: any) => {
+const sanitizeInput = (event: any) => {
   let value = event.target.value;
 
   // Remove non-digit characters
@@ -13,20 +13,20 @@ const sanitizeInput = (index: any, event: any) => {
   value = value.replace(/^0+/, '');
 
   // Update the corresponding item in the array
-  createProjectStore.trackFormFields[index]['price'] = value;
+  projectStore.trackForm['price'] = value;
 };
 
 const addTrack = () => {
-  createProjectStore.addTrack()
+  projectStore.addTrack()
 }
 
 </script>
 
 <template>
   <h1 class="text-2xl font-bold">Add project tracks</h1>
-
+  {{ projectStore.trackForm }}
   <div class="my-4">
-    <button onclick="add_track_modal.showModal()" class="btn btn-secondary text-md">
+    <button onclick="add_track_modal.showModal()" class="btn btn-secondary text-lg">
       <Icon icon="material-symbols:music-note-add-rounded" class="text-xl" />
       Add new track
     </button>
@@ -36,27 +36,24 @@ const addTrack = () => {
     <div class="modal-box lg:translate-x-[150px]">
       <h3 class="text-lg font-bold">Add track</h3>
       <form>
-        <div v-for="(track, t) in createProjectStore.trackFormFields" :key="t">
-          <p class="text-xl font-bold">Track {{ t + 1 }}</p>
-          <div class="mb-4">
-            <label class="form-control w-full">
-              <div class="label flex flex-col items-start">
-                <span class="label-text text-lg font-bold">Title</span>
-                <span class="label-text">Choose a title for your track.</span>
-              </div>
-              <input v-model="track.title" id="title" name="title" type="text" placeholder="Enter track title"
-                class="input input-ghost bg-neutral w-full" />
-            </label>
-          </div>
-          <div class="mb-4">
-            <label class="form-control w-full">
-              <div class="label flex flex-col items-start">
-                <span class="label-text text-lg font-bold">Price</span>
-              </div>
-              <input v-model="track.price" @input="sanitizeInput(t, $event)" id="price" name="price" type="number"
-                placeholder="Enter track price" class="input input-ghost bg-neutral w-full" />
-            </label>
-          </div>
+        <div class="mb-4">
+          <label class="form-control w-full">
+            <div class="label flex flex-col items-start">
+              <span class="label-text text-lg font-bold">Title</span>
+              <span class="label-text">Choose a title for your track.</span>
+            </div>
+            <input v-model="projectStore.trackForm.title" id="title" name="title" type="text"
+              placeholder="Enter track title" class="input input-ghost bg-neutral w-full" />
+          </label>
+        </div>
+        <div class="mb-4">
+          <label class="form-control w-full">
+            <div class="label flex flex-col items-start">
+              <span class="label-text text-lg font-bold">Price</span>
+            </div>
+            <input v-model="projectStore.trackForm.price" @input="sanitizeInput" id="price" name="price" type="number"
+              placeholder="Enter track price" class="input input-ghost bg-neutral w-full" />
+          </label>
         </div>
       </form>
       <div class="modal-action">
