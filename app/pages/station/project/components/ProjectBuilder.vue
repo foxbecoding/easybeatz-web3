614e321f7687689ccb66be292b0165a7d5001a71
foxbecoding <foxbecoding@gmail.com>
2025-02-05 14:52:02 -0600

    removes unused logic

--- app/pages/station/project/components/ProjectBuilder.vue
+++ app/pages/station/project/components/ProjectBuilder.vue
@@ -4,7 +4,6 @@ import { type Mood } from "@/services/models/mood";
 import { useCreateProjectStore } from "@/store/createProject";
 import AlbumForm from "./AlbumForm.vue";
 import TrackForm from "./TrackForm.vue";
-import Review from "./Review.vue";
 
 const props = defineProps<{
   genres: Genre[] | null;
@@ -12,25 +11,14 @@ const props = defineProps<{
 }>();
 
 const createProjectStore = useCreateProjectStore();
-const activeSteps = computed<string[]>(() => createProjectStore.activeSteps);
-const activeStep = computed(() => createProjectStore.step);
 
 </script>
 
 <template>
-  <div class="card bg-neutral max-w-[1200px] w-full shadow-md block mx-auto">
-    <div class="card-body">
-      <ul class="steps w-full">
-        <li class="step" :class="activeSteps.includes('step1') ? 'step-primary' : ''">Project details
-        </li>
-        <li class="step" :class="activeSteps.includes('step2') ? 'step-primary' : ''">Tracks</li>
-        <li class="step" :class="activeSteps.includes('step3') ? 'step-primary' : ''">Review</li>
-      </ul>
-      <div class="pt-4">
-        <AlbumForm v-if="activeStep === 'step1'" />
-        <TrackForm v-if="activeStep === 'step2'" />
-        <Review v-if="activeStep === 'step3'" />
-      </div>
+  <div class="card bg-base-100 max-w-[600px] w-full block mx-auto">
+    <div class="card-body p-0">
+      <AlbumForm />
+      <div class="divider" />
     </div>
   </div>
 
