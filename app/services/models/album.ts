00ab5a899f0ae768ac69f21bec7f814adb43ac7a
foxbecoding <foxbecoding@gmail.com>
2025-02-10 11:51:48 -0600

    implements method to submit album project

--- app/services/models/album.ts
+++ app/services/models/album.ts
@@ -1,9 +1,17 @@
 import { type ApiData } from "@/composables/useApi";
 
+export const submitAlbumProject = async (data: any) => {
+    const config = useRuntimeConfig();
+    const fetchPath = `${config.public.API_ALBUM_PROJECT}/`;
+    const apiData: ApiData = { method: 'POST', path: fetchPath, data, isMultiPart: true };
+    const res = await useApi(apiData);
+    return res;
+}
+
 export const albumFormValidator = async (data: any) => {
     const config = useRuntimeConfig();
     const fetchPath = `${config.public.API_ALBUM_PROJECT}/validate_album_form/`;
     const apiData: ApiData = { method: 'POST', path: fetchPath, data, isMultiPart: true };
     const res = await useApi(apiData);
-    return res
+    return res;
 }
