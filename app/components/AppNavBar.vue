a0f1b0da123f205196deb6b0a07f6e6887a61cfa
foxbecoding <foxbecoding@gmail.com>
2025-02-11 15:42:25 -0600

    adjust layout

--- app/components/AppNavBar.vue
+++ app/components/AppNavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="navbar bg-base-200 fixed left-0 z-50 mx-auto w-full max-w-[2560px] px-4 sm:px-8">
+  <div class="navbar bg-base-100 lg:bg-base-200 fixed left-0 z-50 mx-auto w-full max-w-[2560px] px-4 sm:px-8">
     <div class="navbar-start">
       <NuxtLink :to="{ name: 'index' }">
         <NuxtImg src="/logo.png" width="200px" />
