29f1667f4a619a758408076d7a3f60ccabc07097
foxbecoding <foxbecoding@gmail.com>
2025-02-11 14:48:46 -0600

    adds element

--- app/components/AppNavBar.vue
+++ app/components/AppNavBar.vue
@@ -13,6 +13,12 @@
       </label>
     </div>
 
+    <div class="navbar-end">
+      <AppWalletConnect v-if="!authStore.isAuthenticated" class="mr-4" />
+      <AppAccountMenu v-else class="mr-4" />
+      <NuxtLink :to="{ name: 'cart' }" class="btn btn-neutral mask mask-squircle btn-square">
+        <Icon icon="solar:bag-music-2-bold" class="text-2xl" />
+      </NuxtLink>
     </div>
   </div>
 </template>
