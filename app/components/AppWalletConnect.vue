43aeaa5a8578757196cd7aa6fa529da7e57f75b1
foxbecoding <foxbecoding@gmail.com>
2025-02-11 18:27:36 -0600

    removes old logic

--- app/components/AppWalletConnect.vue
+++ app/components/AppWalletConnect.vue
@@ -56,9 +56,6 @@ const authenticateUser = async (signature: any, message: string) => {
 
 
 <template>
-  <button @click="!isConnecting ? connectWallet() : false" class="btn btn-neutral rounded-[1rem] text-xl"
-    :disabled="isConnecting">
-    <Icon class="text-2xl" icon="solar:wallet-2-bold" />
     Login
     <Icon icon="token-branded:phantom" class="text-2xl" />
   </button>
