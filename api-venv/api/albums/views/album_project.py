97646b09c1120283843177d8f4eee6c2ae90b3f2
foxbecoding <foxbecoding@gmail.com>
2025-02-12 14:04:02 -0600

    removes imports

--- api-venv/api/albums/views/album_project.py
+++ api-venv/api/albums/views/album_project.py
@@ -4,8 +4,6 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from ..services import AlbumProjectService, AlbumFormService
-from users.models import User
-from stations.models import Station
 
 class AlbumProjectViewSet(viewsets.ViewSet):
     def get_permissions(self):
