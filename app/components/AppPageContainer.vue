9a5ff8bd5081498acef597cb336f499df8b54ac9
foxbecoding <foxbecoding@gmail.com>
2025-02-11 19:25:49 -0600

    adjust styling for responsiveness

--- app/components/AppPageContainer.vue
+++ app/components/AppPageContainer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-100 p-[0.5rem]">
+  <div class="w-100 p-[0.5rem] lg:overflow-y-scroll lg:pb-[64px] lg:h-full">
     <div class="mx-auto w-full max-w-[2560px] px-4 sm:px-8 2xl:px-16 py-8">
       <slot />
     </div>
