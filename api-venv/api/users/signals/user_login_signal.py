8a345c34ee667e7ccf9dc995b2cf2c1ad95bc45e
foxbecoding <foxbecoding@gmail.com>
2025-02-13 11:56:30 -0600

    implements signal receiver

--- api-venv/api/users/signals/user_login_signal.py
+++ api-venv/api/users/signals/user_login_signal.py
@@ -3,3 +3,7 @@ from ..models import User, UserLogin
 
 web3_login_done = Signal()
 
+@receiver(web3_login_done)
+def web3_login_done_handler(sender, user: User, **kwargs): 
+    user_login = UserLogin(user = user)
+    user_login.save()
