614a97ae212be3c49893697619c6dbb377c30732
foxbecoding <foxbecoding@gmail.com>
2025-02-14 16:57:10 -0600

    serializes station albums

--- api-venv/api/stations/serializers/public_station_serializer.py
+++ api-venv/api/stations/serializers/public_station_serializer.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from ..models import Station
+from albums.serializers import StationAlbumSerializer
 
 class PublicStationSerializer(serializers.ModelSerializer):
+    albums = StationAlbumSerializer(many=True)
     class Meta:
         model = Station
         fields = [
