23f083d8e003d48b59d43f2898edfbfad9505a0e
foxbecoding <foxbecoding@gmail.com>
2025-02-16 00:13:18 -0600

    refactors fields into serializers

--- api-venv/api/stations/serializers/public_station_serializer.py
+++ api-venv/api/stations/serializers/public_station_serializer.py
@@ -1,19 +1,34 @@
 from rest_framework import serializers
 from ..models import Station
 from albums.serializers import StationAlbumSerializer
+from datetime import datetime
 
 class PublicStationSerializer(serializers.ModelSerializer):
     albums = StationAlbumSerializer(many=True)
+    is_owner = serializers.SerializerMethodField()
+    launch_date = serializers.SerializerMethodField()
+    station_picture = serializers.SerializerMethodField()
+
     class Meta:
         model = Station
         fields = [
             'albums',
-            'created',
             'description',
             'email',
             'handle',
+            'is_owner',
+            'launch_date',
             'name',
-            'picture'
+            'station_picture',
         ]
 
+    def get_is_owner(self, obj):
+        return self.context['is_owner']
 
+    def get_launch_date(self, obj: Station):
+        created_date = datetime.fromisoformat(str(obj.created).replace("Z", "+00:00"))
+        return created_date.year
+
+    def get_station_picture(self, obj):
+        picture = obj.picture.picture
+        return str(picture)
