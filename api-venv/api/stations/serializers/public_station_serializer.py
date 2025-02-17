6e745db4aa5a6edacefcf98fa9b65fd5dd082bd9
foxbecoding <foxbecoding@gmail.com>
2025-02-16 21:57:59 -0600

    modifies serializer logic

--- api-venv/api/stations/serializers/public_station_serializer.py
+++ api-venv/api/stations/serializers/public_station_serializer.py
@@ -1,13 +1,28 @@
 from rest_framework import serializers
-from ..models import Station
-from albums.serializers import StationAlbumSerializer
+from ..models import Station, StationPicture
+from albums.models import Album
 from datetime import datetime
 
+class StationPictureField(serializers.RelatedField):
+    def to_representation(self, value: StationPicture):
+        return str(value.picture.url)
+
+class AlbumsField(serializers.RelatedField):
+    def to_representation(self, value: Album):
+        data = {
+            "aid": value.aid,
+            "bio": value.bio,
+            "title": value.title,
+            "tracks_count": value.tracks.count(),
+            "cover": value.cover.picture.url,
+        }
+        return data
+
 class PublicStationSerializer(serializers.ModelSerializer):
-    albums = StationAlbumSerializer(many=True)
+    albums = AlbumsField(many=True, read_only=True)
     is_owner = serializers.SerializerMethodField()
     launch_date = serializers.SerializerMethodField()
-    station_picture = serializers.SerializerMethodField()
+    picture = StationPictureField(read_only=True)
 
     class Meta:
         model = Station
@@ -19,7 +34,7 @@ class PublicStationSerializer(serializers.ModelSerializer):
             'is_owner',
             'launch_date',
             'name',
-            'station_picture',
+            'picture',
         ]
 
     def get_is_owner(self, obj):
@@ -28,7 +43,3 @@ class PublicStationSerializer(serializers.ModelSerializer):
     def get_launch_date(self, obj: Station):
         created_date = datetime.fromisoformat(str(obj.created).replace("Z", "+00:00"))
         return created_date.year
-
-    def get_station_picture(self, obj):
-        picture = obj.picture.picture
-        return str(picture)
