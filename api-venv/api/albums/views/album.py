b9c0b04050d9be7b70ba2dae6f1e61f788e8e778
foxbecoding <foxbecoding@gmail.com>
2025-02-17 22:24:43 -0600

    adds retrieve method

--- api-venv/api/albums/views/album.py
+++ api-venv/api/albums/views/album.py
@@ -12,3 +12,14 @@ class AlbumViewSet(viewsets.ViewSet):
         if self.action in needs_auth: permission_classes = [IsAuthenticated]
         return [permission() for permission in permission_classes]
 
+    def retrieve(self, request, pk=None):
+        album_qs = Album.objects.prefecth_related(
+            Prefetch(
+                "tracks",
+                queryset=Track.objects
+                .only("bpm", "duration", "tid", "title")
+            )
+        )
+        
+        if not album_qs:
+            return Response({"error": "No Station", "is_owner": is_owner}, status=status.HTTP_404_NOT_FOUND) 
