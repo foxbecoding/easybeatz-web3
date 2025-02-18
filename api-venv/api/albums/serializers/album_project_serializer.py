0e511412ebe32dd128b0fd2ee0163352ae10822d
foxbecoding <foxbecoding@gmail.com>
2025-02-17 22:26:32 -0600

    implements method

--- api-venv/api/albums/serializers/album_project_serializer.py
+++ api-venv/api/albums/serializers/album_project_serializer.py
@@ -19,3 +19,21 @@ class AlbumCoverField(serializers.RelatedField):
         return value.picture.url
 
 class TracksField(serializers.RelatedField):
+    def to_representation(self, value):
+        display: TrackDisplay = value.display
+        price: TrackPrice = value.price
+        exclusive_price: TrackExclusivePrice = value.exclusive_price
+        mood: Mood = value.mood
+
+        data = {
+            "bpm": value.bpm,
+            "duration": value.duration,
+            "tid": value.tid,
+            "title": value.title,
+            "display": display.audio,
+            "price": price.value,
+            "exclusive_price": exclusive_price.value,
+            "mood": { "name": mood.name, "slug": mood.slug }
+        }
+        return data
+
