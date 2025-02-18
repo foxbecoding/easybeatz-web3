aedf01eac48dd7e543c356642d95d8e0c1200716
foxbecoding <foxbecoding@gmail.com>
2025-02-17 22:27:50 -0600

    removes methods

--- api-venv/api/albums/views/album.py
+++ api-venv/api/albums/views/album.py
@@ -8,18 +8,6 @@ from ..models import Album, Track
 class AlbumViewSet(viewsets.ViewSet):
     def get_permissions(self):
         permission_classes = [AllowAny]
-        needs_auth = ['created']
+        needs_auth = ['create']
         if self.action in needs_auth: permission_classes = [IsAuthenticated]
         return [permission() for permission in permission_classes]
-
-    def retrieve(self, request, pk=None):
-        album_qs = Album.objects.prefecth_related(
-            Prefetch(
-                "tracks",
-                queryset=Track.objects
-                .only("bpm", "duration", "tid", "title")
-            )
-        )
-        
-        if not album_qs:
-            return Response({"error": "No Station", "is_owner": is_owner}, status=status.HTTP_404_NOT_FOUND) 
