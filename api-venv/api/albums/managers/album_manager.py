b4afb93d724c01d10c8a3b52983589e139dfce7a
foxbecoding <foxbecoding@gmail.com>
2025-02-18 01:32:02 -0600

    implements manager class for album

---
+++ api-venv/api/albums/managers/album_manager.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.db.models import Prefetch, Sum
+from django.apps import apps
+from genres.models import Genre
+
+class AlbumManager(models.Manager):
+    def with_tracks_and_relations(self, aid):
+        Track = apps.get_model('albums', 'Track')
+
+        return self.select_related(
+            "cover",
+            "station",
+            "station__user"
+        ).only(
+            "aid", "bio", "title",
+            "cover__picture",
+            "station__handle", "station__picture__picture", "station__user__pubkey"
+        ).prefetch_related(
+            Prefetch(
+                "tracks",
+                queryset=Track.objects
+                .prefetch_related(
+                    Prefetch(
+                        "genres",  
+                        queryset=Genre.objects.only("name", "slug")
+                    )
+                )
+                .select_related("display", "exclusive_price", "mood", "price")
+                .only(
+                    "bpm", "duration", "tid", "title",
+                    "display__audio",
+                    "exclusive_price__value",
+                    "mood__name",
+                    "mood__slug",
+                    "price__value",
+                )
+                .order_by("order_no")
+            )
+        ).annotate(
+            total_duration=Sum("tracks__duration")
+        ).filter(aid=aid).first()
+
