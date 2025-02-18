739a59e6a1c4da4d78975cb419ae78ec20cf8db9
foxbecoding <foxbecoding@gmail.com>
2025-02-18 01:37:25 -0600

    removes imports

--- api-venv/api/albums/serializers/album_project_serializer.py
+++ api-venv/api/albums/serializers/album_project_serializer.py
@@ -1,6 +1,4 @@
 from rest_framework import serializers
-from stations.models import Station, StationPicture
-from ..models import Album, AlbumCover, Track, TrackDisplay, TrackPrice, TrackExclusivePrice
 from moods.models import Mood
 from genres.models import Genre
 from datetime import datetime
