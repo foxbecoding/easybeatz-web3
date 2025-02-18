7c709f2b136029245f48d2bb2dd24fcf2de58ea4
foxbecoding <foxbecoding@gmail.com>
2025-02-17 18:16:13 -0600

    removes imports

--- api-venv/api/stations/views/station.py
+++ api-venv/api/stations/views/station.py
@@ -7,8 +7,6 @@ from rest_framework.decorators import action
 from ..serializers import StationSerializer, PublicStationSerializer
 from users.models import User
 from albums.models import Album
-from ..models import Station, StationPicture
-from datetime import datetime
 
 class StationViewSet(viewsets.ViewSet):
     def get_permissions(self):
