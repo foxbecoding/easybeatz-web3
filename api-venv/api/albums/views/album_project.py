cb395d2b14de4d219b100e92f2ff4c306de7c2dc
foxbecoding <foxbecoding@gmail.com>
2025-02-17 22:24:57 -0600

    removes imports

--- api-venv/api/albums/views/album_project.py
+++ api-venv/api/albums/views/album_project.py
@@ -1,7 +1,6 @@
 from django.db.models import Count, Prefetch, Sum
 from rest_framework import viewsets
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from ..services import AlbumProjectService, AlbumFormService
