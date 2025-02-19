5edf9b13bc5d3c54f5227fd63c9852fc6cb87169
foxbecoding <foxbecoding@gmail.com>
2025-02-19 17:33:56 -0600

    adds props

--- app/components/AppStationBlock.vue
+++ app/components/AppStationBlock.vue
@@ -5,6 +5,14 @@ const props = defineProps<{
   station: Station;
 }>();
 
+const config = useRuntimeConfig();
+const stationPicture = computed(() => `${config.public.MEDIA_URL}` + props.station.picture);
+const img = useImage();
+
+const imgStyles = computed(() => {
+  const imgUrl = img(stationPicture.value, { width: 36, height: 36 })
+  return { backgroundImage: `url('${imgUrl}')`, backgroundSize: 'cover', backgroundPosition: 'center' }
+})
 </script>
 
 <template>
