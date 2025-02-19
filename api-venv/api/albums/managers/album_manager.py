7dbd64fd3687577f5fcbb4621b53e94e7f71fcc9
foxbecoding <foxbecoding@gmail.com>
2025-02-19 17:30:41 -0600

    adds import

--- api-venv/api/albums/managers/album_manager.py
+++ api-venv/api/albums/managers/album_manager.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models import Prefetch, Sum
+from django.db.models import Prefetch, Sum, Q
 from django.apps import apps
 from genres.models import Genre
 
