6904c3c5d5101c80c5f64ae333c74479f120e492
foxbecoding <foxbecoding@gmail.com>
2025-02-18 21:16:39 -0600

    adds setup props

--- app/pages/project/[aid].vue
+++ app/pages/project/[aid].vue
@@ -2,6 +2,14 @@
 import { useAuthStore } from "@/store/auth";
 import { type Album } from "@/services/models/album";
 
+const route = useRoute();
+const authStore = useAuthStore();
+const config = useRuntimeConfig();
+const aid = ref(route.params.aid)
+const isAuthenticated = computed(() => authStore.isAuthenticated)
+const fetchPath = `${config.public.API_ALBUM_PROJECT}/${aid.value}/`;
+const demoTracks = Array.from({ length: 12 }, (_, i) => i);
+
 </script>
 <template>
 </template>
