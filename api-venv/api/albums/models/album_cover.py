4316ef365d39a18bc4e62e8a1da3e5c8f5899b27
foxbecoding <foxbecoding@gmail.com>
2025-02-20 11:45:34 -0600

    adds objects manager

--- api-venv/api/albums/models/album_cover.py
+++ api-venv/api/albums/models/album_cover.py
@@ -19,3 +19,4 @@ class AlbumCover(models.Model):
     created = models.DateTimeField(auto_now_add=True, null=True)
     updated = models.DateTimeField(auto_now=True, null=True)
     deleted = models.DateTimeField(null=True)
+    objects = models.Manager()
