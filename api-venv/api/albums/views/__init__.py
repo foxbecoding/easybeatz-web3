bc98b71a91ab8d6fa142eb5b8fda806c9bb8e85d
foxbecoding <foxbecoding@gmail.com>
2025-02-20 21:27:13 -0600

    removes unused view

--- api-venv/api/albums/views/__init__.py
+++ api-venv/api/albums/views/__init__.py
@@ -1,6 +1,5 @@
 from .album import AlbumViewSet
 from .track import TrackViewSet
-from .album_project import AlbumProjectViewSet
 
 
-__all__ = ['AlbumProjectViewSet', 'AlbumViewSet', 'TrackViewSet']
+__all__ = ['AlbumViewSet', 'TrackViewSet']
