ef23ab0cf84ce50cd0fc981471b1701d2f3ed5e1
foxbecoding <foxbecoding@gmail.com>
2025-02-20 21:27:27 -0600

    removes imports

--- api-venv/api/albums/views/album.py
+++ api-venv/api/albums/views/album.py
@@ -1,9 +1,7 @@
-from django.db.models import Count, Prefetch, Sum
 from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
-from ..models import Album, Track
 
 class AlbumViewSet(viewsets.ViewSet):
     def get_permissions(self):
