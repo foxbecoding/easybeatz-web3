e31c94d1a849ec428d463dcc7adbd072377ba7fe
foxbecoding <foxbecoding@gmail.com>
2025-02-21 12:38:03 -0600

    removes import

--- api-venv/api/stations/views/station.py
+++ api-venv/api/stations/views/station.py
@@ -1,4 +1,4 @@
-from django.db.models import Count, Prefetch
+from django.db.models import Prefetch
 from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
