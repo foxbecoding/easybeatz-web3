383202774a81be80f842b962fd73049f87d97515
foxbecoding <foxbecoding@gmail.com>
2025-02-21 15:29:01 -0600

    implements station manager

---
+++ api-venv/api/stations/managers/station_manager.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.db.models import Prefetch, Q
+from django.apps import apps
+
+class StationManager(models.Manager):
+    def with_albums_and_relations(self, user_pubkey):
+        Album = apps.get_model("albums", "Album")
+
+        # Ensure `user_pubkey` is iterable
+        query_filter = Q(user__pubkey=user_pubkey) if isinstance(user_pubkey, str) else Q(user__pubkey__in=user_pubkey)
+
+        queryset = self.select_related(
+            "picture",
+            "user"
+        ).only(
+            "description", "email", "handle", "name",
+            "picture__picture"
+        ).prefetch_related(
+            Prefetch(
+                'albums',
+                queryset=Album.objects
+                .select_related("cover")
+                .prefetch_related('tracks')
+                .only('aid', 'bio', 'title', 'cover__picture')
+            )
+        ).filter(query_filter)
+
+        # Return a single object if user_pubkey was a string, otherwise return the queryset
+        return queryset.first() if isinstance(user_pubkey, str) else queryset
