3adce7d4d1634f29fd633cce15311f7fa227608e
foxbecoding <foxbecoding@gmail.com>
2025-02-24 11:49:26 -0600

    adds component for list

---
+++ app/components/AppTrack/List.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { type Track } from "@/services/models/album";
+import { type Station } from "@/services/models/station";
+
+const props = defineProps<{
+  tracks: Track[];
+  station: Station;
+  albumCover: string;
+}>();
+
+const route = useRoute();
+const img = useImage();
+const albumCover = computed(() => props.albumCover);
+
+const albumCoverStyles = computed(() => {
+  const imgUrl = img(albumCover.value, { width: 108 })
+  return { backgroundImage: `url('${imgUrl}')`, backgroundSize: 'cover', backgroundPosition: 'center' }
+})
+
+const showProjectCover = computed(() => {
+  const hideOnRoutes = ['project-aid']
+  if (hideOnRoutes.includes(String(route.name))) {
+    return false;
+  }
+  return true;
+});
+
+</script>
+
+<template>
+  <div class="flex flex-col gap-4">
+    <div v-for="(track, t) in tracks" :key="t" class="md:h-[128px] bg-base-200 rounded-[1rem]">
+      <div v-if="showProjectCover" :style="albumCoverStyles"
+        class="w-full h-full max-w-[640px] max-h-[640px] aspect-square group relative bg-neutral rounded-t-[0.5rem] flex sm:hidden items-center">
+      </div>
+      <div class="flex flex-col gap-4 md:flex-row justify-between h-full p-4">
+        <div class="flex flex-1 items-center gap-2">
+          <span class="font-semibold">{{ t + 1 }}</span>
+          <div class="flex gap-2 md:gap-4">
+            <div v-if="showProjectCover" :style="albumCoverStyles"
+              class="min-w-[72px] h-[72px] sm:min-w-[108px] sm:h-[108px] group relative bg-neutral rounded-[0.5rem] hidden sm:flex">
+            </div>
+            <div class="flex flex-col gap-1 items-start">
+              <p class="text-lg font-bold line-clamp-1 overflow-hidden text-ellipsis">
+                {{ track.title }}
+              </p>
+              <AppStationBlock :station="station" />
+              <div class="flex gap-4">
+                <span>duration: {{ track.formatted_duration }}</span>
+                <span>BPM: {{ track.bpm }}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="flex flex-col md:flex-row gap-2  items-start md:items-center">
+          <NuxtLink class="btn btn-primary w-full md:w-[116px] rounded-[1rem] order-last md:order-first">
+            Buy ${{ track.price }}
+          </NuxtLink>
+          <AppTrackControls class="order-first md:order-last" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
