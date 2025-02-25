0dc19d7e64db483592c9fdbbeedb07f073237b7b
foxbecoding <foxbecoding@gmail.com>
2025-02-24 21:48:52 -0600

    implements TrackList interface

--- app/store/musicPlayer.ts
+++ app/store/musicPlayer.ts
@@ -1,3 +1,9 @@
 import { type Station } from "@/services/models/station";
 import { type Album, type Track } from "@/services/models/album";
 
+export interface TrackList {
+  album: Album;
+  station: Station;
+  track: Track;
+}
+
