ba5bc2f83b17550903dfb69ed630c395e5084f55
foxbecoding <foxbecoding@gmail.com>
2025-02-25 19:45:16 -0600

    implements component logic

--- app/components/AppPageContainer.vue
+++ app/components/AppPageContainer.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+import { useMusicPlayerStore } from "@/store/musicPlayer";
+
+const musicPlayer = useMusicPlayerStore();
+const containerAdjust = computed(() => {
+  if (musicPlayer.show) {
+    return 'padding-bottom: 128px';
+  }
+
+  return '';
+})
+</script>
+
 <template>
   <div class="w-100 p-[0.5rem] lg:overflow-y-auto lg:h-[calc(100vh-64px)]">
     <div class="mx-auto w-full max-w-[2560px] px-4 sm:px-8 2xl:px-16 py-8">
