a4142673a2da8a5f71c3c84100913dfa3809bea0
foxbecoding <foxbecoding@gmail.com>
2025-02-27 13:52:08 -0600

    fixes mobile styling

--- app/components/AppPageContainer.vue
+++ app/components/AppPageContainer.vue
@@ -12,7 +12,7 @@ const containerAdjust = computed(() => {
 </script>
 
 <template>
-  <div class="w-100 p-[0.5rem] lg:overflow-y-auto h-[calc(100vh-64px)]" :class="musicPlayer.show ? 'pb-[128px]' : ''">
+  <div class="w-100 p-[0.5rem] overflow-y-auto h-[calc(100vh-64px)]" :class="musicPlayer.show ? 'pb-[128px]' : ''">
     <div class="mx-auto w-full max-w-[2560px] px-4 sm:px-8 2xl:px-16 py-8">
       <slot />
     </div>
