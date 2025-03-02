70c59df2975f5491d2715d64c7da5edf5ef303ce
foxbecoding <foxbecoding@gmail.com>
2025-03-02 11:38:31 -0600

    adds test for user login nonce create

--- api-venv/api/users/tests/test_models/test_user_login_nonce.py
+++ api-venv/api/users/tests/test_models/test_user_login_nonce.py
@@ -2,3 +2,15 @@ import pytest
 from users.models import UserLoginNonce
 from users.tests.factories import UserLoginNonceFactory
 
+@pytest.mark.django_db
+def test_create_user_login_nonce():
+    """
+    Ensure a UserLoginNonce model can be created successfully.
+    """
+    user_login_nonce = UserLoginNonce.objects.create(
+        pubkey="D3c6JWSDHXsUCHf8uuQ9raYmDnbnCHTvb5FHHvNrdjPF"
+    )
+    assert user_login_nonce.pk is not None  # Check if saved to the DB
+    assert user_login_nonce.nonce is not None  # Ensure nonce is generated
+    assert len(user_login_nonce.nonce) == 36  # Validate nonce format (UUID)
+
