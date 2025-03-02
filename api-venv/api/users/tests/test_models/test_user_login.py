071f866be327fc1ea21c945e1d63d8575925b1f1
foxbecoding <foxbecoding@gmail.com>
2025-03-02 11:37:37 -0600

    adds test for user_login create

--- api-venv/api/users/tests/test_models/test_user_login.py
+++ api-venv/api/users/tests/test_models/test_user_login.py
@@ -2,3 +2,14 @@ import pytest
 from users.models import UserLogin
 from users.tests.factories import UserFactory, UserLoginFactory
 
+@pytest.mark.django_db
+def test_create_user_login():
+    """
+    Ensure a UserLogin model can be created successfully.
+    """
+    user = UserFactory()
+    user_login = UserLogin.objects.create(user=user)
+    assert user_login.pk is not None  # Check if saved to the DB
+    assert user_login.user == user  # Ensure ForeignKey relationship is set
+
+
