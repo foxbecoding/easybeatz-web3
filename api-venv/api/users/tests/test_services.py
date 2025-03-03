9f1a8a5a536578f8d498b22ef1844bd2cbf23bbb
foxbecoding <foxbecoding@gmail.com>
2025-03-03 11:06:16 -0600

    adds test for successful login

--- api-venv/api/users/tests/test_services.py
+++ api-venv/api/users/tests/test_services.py
@@ -50,3 +50,25 @@ class TestWeb3LoginService:
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data == {"error": "Verification failed"}
 
+    @patch("users.models.UserLoginNonce.objects.filter")
+    @patch("users.utils.web3_login_message_generator")
+    @patch("users.services.Web3LoginService._verify_message", return_value=True)
+    @patch("users.services.Web3LoginService._verify_solana_signature", return_value=True)
+    @patch("users.models.User.objects.get_or_create", return_value=(MagicMock(pubkey="test_pubkey"), True))
+    @patch("rest_framework_simplejwt.tokens.RefreshToken.for_user", return_value=MagicMock(access_token="access_token"))
+    @patch("users.signals.user_login_signal.web3_login_done.send")
+    def test_successful_login(
+        self, mock_signal_send, mock_refresh_token, mock_get_or_create, mock_verify_signature, mock_verify_message, mock_msg_generator, mock_nonce_filter
+    ):
+        """Test successful login"""
+        mock_nonce_filter.return_value.last.return_value = MagicMock(nonce="123456")
+        mock_msg_generator.return_value = "message"
+        
+        data = {"pubkey": "test_pubkey", "originalMessage": "message", "signedMessage": "signature"}
+        service = Web3LoginService(data)
+        
+        response = service.run()
+        
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {"access_token": "access_token", "pubkey": "test_pubkey"}
+        mock_signal_send.assert_called_once()
