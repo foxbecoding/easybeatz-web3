56a69e004e5fc9d9471e66326d73dbb9e94935f4
foxbecoding <foxbecoding@gmail.com>
2025-03-03 12:31:26 -0600

    adds fixture for station

--- api-venv/api/stations/tests/conftest.py
+++ api-venv/api/stations/tests/conftest.py
@@ -5,3 +5,14 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from PIL import Image
 import io
 
+@pytest.fixture
+def default_station(db, default_user):
+    """Create a test station linked to the test user"""
+    return Station.objects.create(
+        user=default_user,
+        name="Test Station",
+        handle="teststation",
+        description="This is a test station.",
+        email="station@example.com",
+    )
+
