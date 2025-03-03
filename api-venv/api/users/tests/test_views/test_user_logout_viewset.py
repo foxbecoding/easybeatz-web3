588eb87dff937c2301e87df9d8dee22a8491455e
foxbecoding <foxbecoding@gmail.com>
2025-03-03 11:04:07 -0600

    adds fixture for client

--- api-venv/api/users/tests/test_views/test_user_logout_viewset.py
+++ api-venv/api/users/tests/test_views/test_user_logout_viewset.py
@@ -4,3 +4,10 @@ from rest_framework.test import APIClient
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.conf import settings
 
+@pytest.fixture
+def client():
+    """Fixture for the API client."""
+    # Ensure HTTP is used for testing, bypassing the HTTPS redirect.
+    settings.SECURE_SSL_REDIRECT = False  # Disable automatic redirect to HTTPS
+    return APIClient()
+
