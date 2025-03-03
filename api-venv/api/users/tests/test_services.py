856545a2751f0390464130310d9c655a8d29a48b
foxbecoding <foxbecoding@gmail.com>
2025-03-03 11:05:13 -0600

    adds test for invalid message

--- api-venv/api/users/tests/test_services.py
+++ api-venv/api/users/tests/test_services.py
@@ -18,3 +18,18 @@ class TestWeb3LoginService:
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data == {"error": "No nonce found for this wallet address"}
 
+    @patch("users.models.UserLoginNonce.objects.filter")
+    @patch("users.utils.web3_login_message_generator")
+    def test_invalid_message_verification(self, mock_msg_generator, mock_nonce_filter):
+        """Test when the message verification fails"""
+        mock_nonce_filter.return_value.last.return_value = MagicMock(nonce="123456")
+        mock_msg_generator.return_value = "wrong_message"
+        
+        data = {"pubkey": "test_pubkey", "originalMessage": "message", "signedMessage": "signature"}
+        service = Web3LoginService(data)
+        
+        response = service.run()
+        
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data == {"error": "Invalid signing message"}
+
