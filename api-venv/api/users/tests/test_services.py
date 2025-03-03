8c0a98187b1171f7901da58544a72571e559127f
foxbecoding <foxbecoding@gmail.com>
2025-03-03 11:04:48 -0600

    adds test class

--- api-venv/api/users/tests/test_services.py
+++ api-venv/api/users/tests/test_services.py
@@ -3,3 +3,6 @@ from unittest.mock import patch, MagicMock
 from rest_framework import status
 from users.services import Web3LoginService
 
+@pytest.mark.django_db
+class TestWeb3LoginService:
+    
