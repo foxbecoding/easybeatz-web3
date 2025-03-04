849087e4fea87dcb103930d3d09f1394dedb1ae6
foxbecoding <foxbecoding@gmail.com>
2025-03-04 13:02:59 -0600

    imports fixtures

--- api-venv/api/stations/tests/test_managers.py
+++ api-venv/api/stations/tests/test_managers.py
@@ -3,10 +3,8 @@ from django.apps import apps
 from django.utils.timezone import now
 from users.models import User
 from users.tests.conftest import default_user
+from albums.tests.conftest import default_album, default_track
 from stations.models import Station, StationPicture
-from albums.models import Album, Track
-
-
 @pytest.fixture
 def album(db, station):
     """Fixture to create an album linked to a station."""
