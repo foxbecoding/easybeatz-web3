25b21a678d729713fbe5c3df40574eb06b088669
foxbecoding <foxbecoding@gmail.com>
2025-03-04 12:39:32 -0600

    adds test for station picture url

--- api-venv/api/stations/tests/test_models/test_station.py
+++ api-venv/api/stations/tests/test_models/test_station.py
@@ -24,10 +24,10 @@ def test_station_create(db, default_user):
 #     assert default_station.picture_url is None
 
 
-# @pytest.mark.django_db
-# def test_picture_url_property_with_picture(default_station_with_relations):
-#     """Test that picture_url property returns the correct URL when picture exists."""
-#     assert default_station_with_relations.picture_url is not None
+@pytest.mark.django_db
+def test_picture_url_property_with_picture(default_station, default_station_picture):
+    """Test that picture_url property returns the correct URL when picture exists."""
+    assert default_station.picture_url is not None
 
 @pytest.mark.django_db
 def test_formatted_launched_date(default_station):
