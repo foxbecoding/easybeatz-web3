f7e4e3cb3f797f3000f973c2816805c97094866b
foxbecoding <foxbecoding@gmail.com>
2025-03-04 13:06:24 -0600

    adds test to test with relations

--- api-venv/api/stations/tests/test_managers.py
+++ api-venv/api/stations/tests/test_managers.py
@@ -9,14 +9,15 @@ from stations.tests.factories import StationFactory
 
 
 @pytest.mark.django_db
-def test_with_albums_and_relations_single(default_station, album, track):
+def test_with_albums_and_relations_single(default_station, default_user, default_album, default_track):
     """Test that `with_albums_and_relations` correctly fetches a single station with related data."""
-    station_with_relations = Station.stations.with_albums_and_relations("test_pubkey")
+     
+    station_with_relations = Station.stations.with_albums_and_relations(default_user.pubkey)
 
     assert station_with_relations is not None
     assert station_with_relations.name == "Test Station"
-    assert station_with_relations.user.pubkey == "test_pubkey"
-    
+    assert station_with_relations.user.pubkey == default_user.pubkey
+
     # Ensure albums are prefetched
     assert station_with_relations.albums.count() == 1
     assert station_with_relations.albums.first().title == "Test Album"
