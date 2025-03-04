10f03617d6e402f132f97cc8e93e4418846695a2
foxbecoding <foxbecoding@gmail.com>
2025-03-04 12:38:00 -0600

    creates album fixture

--- api-venv/api/albums/tests/conftest.py
+++ api-venv/api/albums/tests/conftest.py
@@ -4,3 +4,12 @@ from albums.models import Album, Track
 from genres.models import Genre
 from moods.models import Mood
 
+@pytest.fixture
+def default_album(db, default_station):
+    """Fixture to create a test album."""
+    return Album.objects.create(
+        station=default_station,
+        title="Test Album",
+        bio="This is a test album.",
+    )
+
