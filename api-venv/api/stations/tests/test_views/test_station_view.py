21380dbc7fb69dfdb22d792de90ad296f9e4d908
foxbecoding <foxbecoding@gmail.com>
2025-03-05 12:08:08 -0600

    adds fixture

--- api-venv/api/stations/tests/test_views/test_station_view.py
+++ api-venv/api/stations/tests/test_views/test_station_view.py
@@ -12,3 +12,8 @@ logger = logging.getLogger("stations")
 @pytest.mark.django_db
 class TestStationViewSet:
     
+    @pytest.fixture
+    def client(self):
+        settings.SECURE_SSL_REDIRECT = False  # Disable automatic redirect to HTTPS
+        return APIClient()
+
