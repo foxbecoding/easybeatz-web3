ff3451f4a1278e667cbbd6e904af27eada0e8eb3
foxbecoding <foxbecoding@gmail.com>
2025-03-05 12:06:08 -0600

    sets user field to read only

--- api-venv/api/stations/serializers/station_serializer.py
+++ api-venv/api/stations/serializers/station_serializer.py
@@ -6,11 +6,14 @@ class StationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Station
         fields = [
+            'user',
             'name', 
             'handle', 
             'description', 
             'email',
         ]
+
+        extra_kwargs = {"user": {"read_only": True}}
 
     def validate(self, attrs):
         handle = attrs['handle']
