14eef94580d05c4b95fb6d87d400030e96cb2be7
foxbecoding <foxbecoding@gmail.com>
2025-03-07 15:43:26 -0600

    adds fixture

--- api-venv/api/albums/tests/test_models/test_track_mp3_model.py
+++ api-venv/api/albums/tests/test_models/test_track_mp3_model.py
@@ -2,3 +2,8 @@ import pytest
 from albums.models import TrackMp3
 from django.core.files.uploadedfile import SimpleUploadedFile
 
+@pytest.fixture
+def track(db, default_track):
+    """Creates track fixture"""
+    return default_track
+
