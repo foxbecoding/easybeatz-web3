0cb6f26330354f3df50f6ef4f2f88b926f92d543
foxbecoding <foxbecoding@gmail.com>
2025-03-07 15:44:35 -0600

    adds test case

--- api-venv/api/albums/tests/test_models/test_track_wav_model.py
+++ api-venv/api/albums/tests/test_models/test_track_wav_model.py
@@ -12,3 +12,17 @@ def track_wav(db, default_track_wav):
     """Creates track_wav fixture"""
     return default_track_wav
 
+@pytest.mark.django_db
+def test_track_wav_create(db, track, test_wav_file):
+    """Creates a TrackWav instance with a test Wav file."""
+    with open(test_wav_file, "rb") as wav:
+        uploaded_audio = SimpleUploadedFile("test_display.wav", wav.read(), content_type="audio/wav")
+
+    """Test TrackWav creation"""
+    track_wav = TrackWav.objects.create(
+        track=track,
+        audio=uploaded_audio
+    )
+    
+    assert track_wav is not None
+
