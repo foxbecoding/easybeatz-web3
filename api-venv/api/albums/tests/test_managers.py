cdcc79287847b25e953c7ba8b4bd5060213b5407
foxbecoding <foxbecoding@gmail.com>
2025-03-07 15:46:04 -0600

    adds fixture

--- api-venv/api/albums/tests/test_managers.py
+++ api-venv/api/albums/tests/test_managers.py
@@ -6,3 +6,7 @@ from albums.models import Album
 def album(db, default_album):
     return default_album
 
+@pytest.fixture
+def album_cover(db, default_album_cover):
+    return default_album_cover
+
