26cedf5a39a1263098809eada1aeda98074a941a
foxbecoding <foxbecoding@gmail.com>
2025-03-07 15:42:44 -0600

    adds fixture

--- api-venv/api/albums/tests/test_models/test_track_model.py
+++ api-venv/api/albums/tests/test_models/test_track_model.py
@@ -3,3 +3,8 @@ from albums.models import Track
 from genres.tests.conftest import default_genre
 from moods.tests.conftest import default_mood
 
+@pytest.fixture
+def genre(db, default_genre):
+    """Creates genre fixture"""
+    return default_genre
+
