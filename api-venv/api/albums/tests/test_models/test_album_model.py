f972961df9adcf80df0bda83b96a50c04af67550
foxbecoding <foxbecoding@gmail.com>
2025-03-07 15:41:25 -0600

    adds fixture

--- api-venv/api/albums/tests/test_models/test_album_model.py
+++ api-venv/api/albums/tests/test_models/test_album_model.py
@@ -2,3 +2,8 @@ import pytest
 from albums.models import Album, AlbumCover
 from stations.tests.conftest import default_station
 
+@pytest.fixture
+def station(db, default_station):
+    """Creates Station fixture"""
+    return default_station
+
