4ad6e63db3e5f6c1dacc865e89935ab068535e12
foxbecoding <foxbecoding@gmail.com>
2025-03-07 15:42:33 -0600

    adds test case

--- api-venv/api/albums/tests/test_models/test_track_exclusive_price_model.py
+++ api-venv/api/albums/tests/test_models/test_track_exclusive_price_model.py
@@ -11,3 +11,14 @@ def track_exclusive_price(db, default_track_exclusive_price):
     """Creates track_exclusive_price fixture"""
     return default_track_exclusive_price
 
+@pytest.mark.django_db
+def test_track_exclusive_price_create(db, track):
+    """Test TrackExclusivePrice creation"""
+    track_exclusive_price = TrackExclusivePrice.objects.create(
+        track=track,
+        value=360
+    )
+    
+    assert track_exclusive_price is not None
+    assert track_exclusive_price.value == 360
+
