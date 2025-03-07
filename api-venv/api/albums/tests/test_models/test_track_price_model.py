f781b66e82afc348cd80d122214d3b1436aadc3e
foxbecoding <foxbecoding@gmail.com>
2025-03-07 15:43:53 -0600

    adds test case

--- api-venv/api/albums/tests/test_models/test_track_price_model.py
+++ api-venv/api/albums/tests/test_models/test_track_price_model.py
@@ -11,3 +11,14 @@ def track_price(db, default_track_price):
     """Creates track_price fixture"""
     return default_track_price
 
+@pytest.mark.django_db
+def test_track_price_create(db, track):
+    """Test TrackPrice creation"""
+    track_price = TrackPrice.objects.create(
+        track=track,
+        value=36
+    )
+    
+    assert track_price is not None
+    assert track_price.value == 36
+
