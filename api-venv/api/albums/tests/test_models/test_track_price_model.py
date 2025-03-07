ed12f2a17dd907c68643a20e166d431e2877b493
foxbecoding <foxbecoding@gmail.com>
2025-03-07 15:43:48 -0600

    adds fixture

--- api-venv/api/albums/tests/test_models/test_track_price_model.py
+++ api-venv/api/albums/tests/test_models/test_track_price_model.py
@@ -6,3 +6,8 @@ def track(db, default_track):
     """Creates track fixture"""
     return default_track
 
+@pytest.fixture
+def track_price(db, default_track_price):
+    """Creates track_price fixture"""
+    return default_track_price
+
