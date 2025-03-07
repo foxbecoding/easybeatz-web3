f539cafe2538256376b4740456073f3a4699c362
foxbecoding <foxbecoding@gmail.com>
2025-03-07 15:45:49 -0600

    adds fixture

--- api-venv/api/albums/tests/conftest.py
+++ api-venv/api/albums/tests/conftest.py
@@ -123,3 +123,11 @@ def default_track_mp3(db, default_track, test_mp3_file):
 
     return TrackMp3.objects.create(track=default_track, audio=uploaded_audio)
 
+@pytest.fixture
+def default_track_wav(db, default_track, test_wav_file):
+    """Creates a TrackWav instance with a test WAV file."""
+    with open(test_wav_file, "rb") as wav:
+        uploaded_audio = SimpleUploadedFile("test_wav.wav", wav.read(), content_type="audio/wav")
+
+    return TrackWav.objects.create(track=default_track, audio=uploaded_audio)
+
