a48fd50950601968b1e4caee9ed0112c07f9924b
foxbecoding <foxbecoding@gmail.com>
2025-03-07 15:43:29 -0600

    adds fixture

--- api-venv/api/albums/tests/test_models/test_track_mp3_model.py
+++ api-venv/api/albums/tests/test_models/test_track_mp3_model.py
@@ -7,3 +7,8 @@ def track(db, default_track):
     """Creates track fixture"""
     return default_track
 
+@pytest.fixture
+def track_mp3(db, default_track_mp3):
+    """Creates track_mp3 fixture"""
+    return default_track_mp3
+
