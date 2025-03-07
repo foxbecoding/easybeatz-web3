c0b12a6ff657247140d7434366bde6e2de625b3e
foxbecoding <foxbecoding@gmail.com>
2025-03-07 15:44:16 -0600

    adds test case

--- api-venv/api/albums/tests/test_models/test_track_stem_model.py
+++ api-venv/api/albums/tests/test_models/test_track_stem_model.py
@@ -12,3 +12,18 @@ def track_stem(db, default_track_stem):
     """Creates track_stem fixture"""
     return default_track_stem
 
+@pytest.mark.django_db
+def test_track_stem_create(db, track, test_wav_file):
+    """Creates a TrackStem instance with a test Wav file."""
+    with open(test_wav_file, "rb") as wav:
+        uploaded_audio = SimpleUploadedFile("test_display.wav", wav.read(), content_type="audio/wav")
+
+    """Test TrackStem creation"""
+    track_stem = TrackStem.objects.create(
+        track=track,
+        name="snare",
+        audio=uploaded_audio
+    )
+    
+    assert track_stem is not None
+
