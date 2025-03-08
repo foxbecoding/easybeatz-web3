75750748ffc5ca49330ae8070e3dc650c115f14a
foxbecoding <foxbecoding@gmail.com>
2025-03-08 14:56:14 -0600

    refactors test case

--- api-venv/api/albums/tests/test_models/test_track_mp3_model.py
+++ api-venv/api/albums/tests/test_models/test_track_mp3_model.py
@@ -15,13 +15,13 @@ def track_mp3(db, default_track_mp3):
 @pytest.mark.django_db
 def test_track_mp3_create(db, track, test_mp3_file):
     """Creates a TrackMp3 instance with a test MP3 file."""
-    with open(test_mp3_file, "rb") as mp3:
-        uploaded_audio = SimpleUploadedFile("test_display.mp3", mp3.read(), content_type="audio/mp3")
+    # with open(test_mp3_file, "rb") as mp3:
+    #     uploaded_audio = SimpleUploadedFile("test_display.mp3", mp3.read(), content_type="audio/mp3")
 
     """Test TrackMp3 creation"""
     track_mp3 = TrackMp3.objects.create(
         track=track,
-        audio=uploaded_audio
+        audio=test_mp3_file
     )
     
     assert track_mp3 is not None
