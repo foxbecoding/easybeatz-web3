4e7f74f2f518aade4215bbb5919aff925cc2ab39
foxbecoding <foxbecoding@gmail.com>
2025-03-09 19:00:35 -0500

    adds method

--- app/services/models/album.ts
+++ app/services/models/album.ts
@@ -40,3 +40,11 @@ export const submitAlbumWithTracks = async (data: any) => {
     const res = await useApi(apiData);
     return res;
 }
+export const submitTrackFavorite = async (trackTid: string) => {
+    const config = useRuntimeConfig();
+    const fetchPath = `${config.public.API_TRACK_FAVORITE}/`;
+    const apiData: ApiData = { method: 'POST', path: fetchPath, data: { track: trackTid } };
+    const res = await useApi(apiData);
+    return res;
+}
+
