076099ac38b70df196d6c3d63c1d69087034acf8
foxbecoding <foxbecoding@gmail.com>
2025-03-12 22:53:38 -0500

    refactors view

--- api-venv/api/users/views/logout.py
+++ api-venv/api/users/views/logout.py
@@ -1,14 +1,14 @@
 from django.contrib.auth import logout
 from rest_framework import viewsets
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
+from core.mixins import ResponseMixin
 
-class UserLogoutViewSet(viewsets.ViewSet):
+class UserLogoutViewSet(viewsets.ViewSet, ResponseMixin):
     def get_permissions(self):
         permission_classes = [IsAuthenticated]
         return [permission() for permission in permission_classes] 
 
     def create(self, request):
         logout(request)
-        return Response(None, status=status.HTTP_200_OK) 
+        return self.view_response("Logged out successfully", None, status.HTTP_200_OK) 
