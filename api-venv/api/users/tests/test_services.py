74381651630b4d3f10d531b508c24c5b643a6b83
foxbecoding <foxbecoding@gmail.com>
2025-03-12 22:53:10 -0500

    refactors test case

--- api-venv/api/users/tests/test_services.py
+++ api-venv/api/users/tests/test_services.py
@@ -58,14 +58,19 @@ class TestWeb3LoginService:
     @patch("users.services.Web3LoginService._verify_message", return_value=True)
     @patch("users.services.Web3LoginService._verify_solana_signature", return_value=True)
     @patch("users.models.User.objects.get_or_create", return_value=(MagicMock(pubkey="test_pubkey"), True))
+    @patch("albums.models.TrackFavorite.objects")
     @patch("rest_framework_simplejwt.tokens.RefreshToken.for_user", return_value=MagicMock(access_token="access_token"))
     @patch("users.signals.user_login_signal.web3_login_done.send")
     def test_successful_login(
-        self, mock_signal_send, mock_refresh_token, mock_get_or_create, mock_verify_signature, mock_verify_message, mock_msg_generator, mock_nonce_filter
+        self, mock_signal_send, mock_refresh_token, mock_track_favorites, mock_get_or_create,  mock_verify_signature, mock_verify_message, mock_msg_generator, mock_nonce_filter
     ):
         """Test successful login"""
         mock_nonce_filter.return_value.last.return_value = MagicMock(nonce="123456")
         mock_msg_generator.return_value = "message"
+
+        mock_filter = MagicMock()
+        mock_filter.values_list.return_value = []
+        mock_track_favorites.filter.return_value = mock_filter
         
         data = {"pubkey": "test_pubkey", "originalMessage": "message", "signedMessage": "signature"}
         service = Web3LoginService(data)
@@ -73,5 +78,6 @@ class TestWeb3LoginService:
         response = service.run()
         
         assert response.status_code == status.HTTP_200_OK
-        assert response.data == {"access_token": "access_token", "pubkey": "test_pubkey"}
+        assert response.data.get("message") == "Logged in successfully!"
+        assert response.data.get("data") == {"access_token": "access_token", "pubkey": "test_pubkey", "favorite_tracks": []}
         mock_signal_send.assert_called_once()
