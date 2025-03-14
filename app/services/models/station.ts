12a156bf065dafaba01e52e66eebe6721b91e491
foxbecoding <foxbecoding@gmail.com>
2025-03-13 21:10:20 -0500

    refactors method

--- app/services/models/station.ts
+++ app/services/models/station.ts
@@ -54,15 +54,11 @@ export const updateStation = async (pubkey: string, data: object): Promise<Stati
 }
 
 export const uploadStationPicture = async (data: object) => {
-    try {
-        const config = useRuntimeConfig();
-        const fetchPath = `${config.public.API_STATION_PICTURE}/upload/`;
-        const apiData: ApiData = { method: 'POST', path: fetchPath, data, isMultiPart: true };
-        const res = await useApi(apiData);
-        return res
-    } catch (e: any) {
-        console.error(e);
-    }
+    const config = useRuntimeConfig();
+    const fetchPath = `${config.public.API_STATION_PICTURE}/upload/`;
+    const apiData: ApiData = { method: 'POST', path: fetchPath, data, isMultiPart: true };
+    const res = await useApi(apiData);
+    return res
 }
 
 export const hasStationChecker = async (): Promise<boolean> => {
