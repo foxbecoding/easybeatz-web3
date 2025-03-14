03d313935984a9a2cb2169fe9ce13537a8280cd2
foxbecoding <foxbecoding@gmail.com>
2025-03-14 15:06:07 -0500

    modifies test case

--- api-venv/api/stations/tests/test_views/test_station_view.py
+++ api-venv/api/stations/tests/test_views/test_station_view.py
@@ -121,7 +121,7 @@ class TestStationViewSet:
         assert response.data.get("message") is None
         assert response.data.get("data") == True
 
-    def test_retrieve_with_albums_and_relations_error(self, client, user, station, station_picture, album, album_cover, track, default_genre, default_mood, test_img_file):
+    def test_retrieve_with_albums_and_relations(self, client, user, station, station_picture, album, album_cover, track, default_genre, default_mood, test_img_file):
         """Test retrieving a station with albums and relations."""
         client.force_authenticate(user=user)
         url = reverse("station-retrieve-with-albums-and-relations", kwargs={"pk": user.pubkey})
@@ -129,7 +129,8 @@ class TestStationViewSet:
         response = client.get(url)
 
         assert response.status_code == 200
-        assert "error" not in response.data  # Should not return an error
+        assert response.data.get("message") is None
+        assert response.data.get("data") is not None
 
     def test_retrieve_with_albums_and_relations(self, client, user, station, station_picture, album, album_cover, track, default_genre, default_mood, test_img_file):
         """Test retrieving a station with albums and relations."""
