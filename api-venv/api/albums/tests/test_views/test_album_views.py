5ab4e65b6c91d16de6674c62bf256c1b199de231
foxbecoding <foxbecoding@gmail.com>
2025-03-17 20:53:22 -0500

    adds imports

--- api-venv/api/albums/tests/test_views/test_album_views.py
+++ api-venv/api/albums/tests/test_views/test_album_views.py
@@ -3,11 +3,12 @@ from rest_framework.test import APIClient
 from rest_framework import status
 from django.urls import reverse
 from django.conf import settings
-from albums.models import Album, Track, TrackStem, TrackExclusivePrice
+from albums.models import Album, Track
 from genres.tests.conftest import default_genre
 from moods.tests.conftest import default_mood
 from stations.tests.conftest import default_station, default_station_picture
 from users.tests.conftest import default_user
+from users.tests.factories import UserFactory
 import logging
 
 logger = logging.getLogger("albums")
