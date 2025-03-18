f3dfabf5f9b10819e4b62c624be73b0d9a80fdf8
foxbecoding <foxbecoding@gmail.com>
2025-03-17 20:53:54 -0500

    adds viewset

--- api-venv/api/albums/views/__init__.py
+++ api-venv/api/albums/views/__init__.py
@@ -1,5 +1,6 @@
 from .album import AlbumViewSet
+from .album_cover import AlbumCoverViewSet
 from .track import TrackViewSet
 from .track_favorite import TrackFavoriteViewSet
 
-__all__ = ['AlbumViewSet', 'TrackViewSet', 'TrackFavoriteViewSet']
+__all__ = ['AlbumViewSet', 'AlbumCoverViewSet', 'TrackViewSet', 'TrackFavoriteViewSet']
