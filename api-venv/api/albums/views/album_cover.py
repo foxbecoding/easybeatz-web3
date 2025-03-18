3cf238d56465b8fb4097afcb2414e517acf715b6
foxbecoding <foxbecoding@gmail.com>
2025-03-17 20:54:47 -0500

    adds view class

--- api-venv/api/albums/views/album_cover.py
+++ api-venv/api/albums/views/album_cover.py
@@ -9,3 +9,18 @@ from core.mixins import ResponseMixin
 import logging
 
 logger = logging.getLogger("albums")
+class AlbumCoverViewSet(viewsets.ViewSet, ResponseMixin):
+    def get_permissions(self):
+        permission_classes = [IsAuthenticated, HasStation]
+        if self.action == 'update':
+            permission_classes = [IsAuthenticated, HasStation, AlbumOwner]
+        return [permission() for permission in permission_classes]
+
+    def update(self, request, pk=None):
+        album = Album.objects.get(aid=pk)
+        album_cover = AlbumCover.objects.get(album=album)
+        serializer = AlbumCoverEditSerializer(album_cover, data=request.data)
+        if not serializer.is_valid():
+            return self.view_response("Failed to update album cover", serializer.errors, status.HTTP_400_BAD_REQUEST)
+        serializer.save()
+        return self.view_response("Album cover updated successfully", None, status.HTTP_202_ACCEPTED)
