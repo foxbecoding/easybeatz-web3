e4889276deb1b062a6b0a76fa702b93512b6f0aa
foxbecoding <foxbecoding@gmail.com>
2025-03-18 21:16:32 -0500

    adds url

--- api-venv/api/albums/urls.py
+++ api-venv/api/albums/urls.py
@@ -5,5 +5,6 @@ router = DefaultRouter()
 router.register(r"album", AlbumViewSet, basename="album")
 router.register(r"album-cover", AlbumCoverViewSet, basename="album-cover")
 router.register(r"track", TrackViewSet, basename="track")
+router.register(r"track-price", TrackPriceViewSet, basename="track-price")
 router.register(r"track-favorite", TrackFavoriteViewSet, basename="track-favorite")
 urlpatterns = router.urls
