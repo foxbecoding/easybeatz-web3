6eb2fbb618dc9018e70aeed31f4fd068f3219b1d
foxbecoding <foxbecoding@gmail.com>
2025-03-18 21:12:43 -0500

    adds serializer class

--- api-venv/api/albums/serializers/track_edit_form_serializer.py
+++ api-venv/api/albums/serializers/track_edit_form_serializer.py
@@ -1,3 +1,13 @@
 from rest_framework import serializers
 from albums.models import Track, TrackPrice, TrackExclusivePrice
 
+class TrackEditFormSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Track
+        fields = [
+            'title',
+            'genres',
+            'mood',
+            'bpm',
+        ]
+
