6d847a79c5f99b1ef5e8e571cd3bea973bf96b77
foxbecoding <foxbecoding@gmail.com>
2025-03-20 22:50:43 -0500

    adds class and permissions

--- api-venv/api/albums/views/track_exclusive_price.py
+++ api-venv/api/albums/views/track_exclusive_price.py
@@ -10,3 +10,14 @@ import logging
 
 logger = logging.getLogger("albums")
 
+class TrackExclusivePriceViewSet(viewsets.ViewSet, ResponseMixin):
+    def get_permissions(self):
+        permission_classes = [AllowAny]
+        needs_auth = ['update', 'destroy']
+        track_owner_actions = ['update', 'destroy']
+        if self.action in needs_auth:
+            permission_classes = [IsAuthenticated, HasStation]
+            if self.action in track_owner_actions:
+                permission_classes = [IsAuthenticated, HasStation, TrackOwner]
+        return [permission() for permission in permission_classes]
+
