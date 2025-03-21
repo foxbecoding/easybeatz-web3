ef6380f4ddf8f5e2004f1555e8de675337d34f50
foxbecoding <foxbecoding@gmail.com>
2025-03-20 22:50:15 -0500

    adds view

--- api-venv/api/albums/views/__init__.py
+++ api-venv/api/albums/views/__init__.py
@@ -3,5 +3,6 @@ from .album_cover import AlbumCoverViewSet
 from .track import TrackViewSet
 from .track_favorite import TrackFavoriteViewSet
 from .track_price import TrackPriceViewSet
+from .track_exclusive_price import TrackExclusivePriceViewSet
 
-__all__ = ['AlbumViewSet', 'AlbumCoverViewSet', 'TrackViewSet', 'TrackPriceViewSet', 'TrackFavoriteViewSet']
+__all__ = ['AlbumViewSet', 'AlbumCoverViewSet', 'TrackViewSet', 'TrackPriceViewSet', 'TrackFavoriteViewSet', 'TrackExclusivePriceViewSet']
