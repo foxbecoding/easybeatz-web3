e587a76574ace7f06bdc007ce3c689f75f4f84cf
foxbecoding <foxbecoding@gmail.com>
2025-03-20 22:50:56 -0500

    adds method to update or create

--- api-venv/api/albums/views/track_exclusive_price.py
+++ api-venv/api/albums/views/track_exclusive_price.py
@@ -21,3 +21,23 @@ class TrackExclusivePriceViewSet(viewsets.ViewSet, ResponseMixin):
                 permission_classes = [IsAuthenticated, HasStation, TrackOwner]
         return [permission() for permission in permission_classes]
 
+    def update(self, request, pk=None):
+        track_tid = pk
+        track_exclusive_price = TrackExclusivePrice.objects.filter(track__tid=track_tid).first()
+        # Create if not present
+        if not track_exclusive_price:
+            track = Track.objects.get(tid=track_tid)
+            request_data = {"track": str(track.pk), "value": request.data['value']}
+            serializer = CreateTrackExclusivePriceSerializer(data=request_data)
+            if not serializer.is_valid():
+                return self.view_response("Failed to add exclusive price", serializer.errors, status.HTTP_400_BAD_REQUEST)
+            serializer.save()
+            return self.view_response("Exclusive price added successfully", None, status.HTTP_201_CREATED)
+
+        # Update if present
+        serializer = TrackExclusivePriceEditFormSerializer(track_exclusive_price, data=request.data)
+        if not serializer.is_valid():
+            return self.view_response("Failed to update exclusive price", serializer.errors, status.HTTP_400_BAD_REQUEST)
+        serializer.save()
+        return self.view_response("Exclusive price updated successfully", None, status.HTTP_202_ACCEPTED)
+
