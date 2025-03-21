63c2370ea60fcf864c823382256a2902e2429f5b
foxbecoding <foxbecoding@gmail.com>
2025-03-20 22:49:23 -0500

    adds test fixture

--- api-venv/api/albums/tests/test_views/test_track_exclusive_price_views.py
+++ api-venv/api/albums/tests/test_views/test_track_exclusive_price_views.py
@@ -28,3 +28,7 @@ class TestTrackExclusivePriceViewSet:
     def album(self, default_album):
         return default_album
 
+    @pytest.fixture
+    def track(self, default_track):
+        return default_track
+
