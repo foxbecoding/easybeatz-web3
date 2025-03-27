d6557dfc265ad20a2ab185c78e16e96692aebbd2
foxbecoding <foxbecoding@gmail.com>
2025-03-26 21:24:09 -0500

    adds numbersOnlyInput method

--- app/utils/helper-methods.ts
+++ app/utils/helper-methods.ts
@@ -7,3 +7,21 @@ export function isEmpty(value: any) {
     (typeof value === "object" && value !== null && Object.keys(value).length === 0)
   );
 }
+
+export const numbersOnlyInput = (event: any) => {
+  const invalidChars = ['+', '-', '.'];
+
+  if (invalidChars.includes(event.key)) {
+    event.preventDefault(); // Block '+' and '-' from being entered
+    return;
+  }
+
+  let value = event.target.value;
+
+  // Remove non-digit characters (just in case)
+  value = value.replace(/\D/g, '');
+
+  // Remove leading zeros
+  value = value.replace(/^0+/, '');
+};
+
