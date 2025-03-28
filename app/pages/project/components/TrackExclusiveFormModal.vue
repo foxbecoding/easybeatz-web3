aee9a800c4f9e316ec8338b8d2d23f38e9382ca8
foxbecoding <foxbecoding@gmail.com>
2025-03-28 14:07:05 -0500

    adds modal control logic

--- app/pages/project/components/TrackExclusiveFormModal.vue
+++ app/pages/project/components/TrackExclusiveFormModal.vue
@@ -5,6 +5,17 @@ const props = defineProps<{
   track: Track;
 }>();
 
+// Modal control logic
+const model = defineModel({ default: false, required: true });
+const closeModal = () => model.value = false;
+watch(model, (newVal) => {
+  if (newVal) {
+    document.getElementById('track_exclusive_modal')?.showModal();
+  } else {
+    document.getElementById('track_exclusive_modal')?.close();
+  }
+});
+
 </script>
 
 <template>
