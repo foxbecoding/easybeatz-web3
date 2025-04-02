f4a6f50a32717adf8eb95d180c5db479cc637ed5
foxbecoding <foxbecoding@gmail.com>
2025-04-01 19:38:08 -0500

    adds interface

--- app/components/AppTrackFormModal.vue
+++ app/components/AppTrackFormModal.vue
@@ -2,6 +2,12 @@
 import { type Genre } from "@/services/models/genre";
 import { type Mood } from "@/services/models/mood";
 
+//component types and interfaces
+interface TrackStemForm {
+  name: string;
+  file: File | null;
+}
+
 </script>
 
 <template>
