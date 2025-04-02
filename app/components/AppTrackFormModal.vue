eef044acc1706fe595bdba36c25d9e2fecebd8a3
foxbecoding <foxbecoding@gmail.com>
2025-04-01 19:44:06 -0500

    adds form html elements

--- app/components/AppTrackFormModal.vue
+++ app/components/AppTrackFormModal.vue
@@ -188,4 +188,184 @@ const formDataPreparer = (formData: FormData) => {
 </script>
 
 <template>
+
+  <dialog id="app_track_form_modal" class="modal modal-bottom sm:modal-middle">
+    <div class="modal-box">
+      <h2 class="text-2xl font-bold">{{ formTitle }}</h2>
+      <form id="track-form" ref="trackForm">
+        <section class="flex flex-col gap-2 mt-4">
+          <h3 class="text-xl font-bold">Details</h3>
+          <label class="form-control w-full">
+            <div class="label flex flex-col items-start">
+              <span class="label-text text-lg font-semibold">Title</span>
+              <span class="label-text">Choose a title for your track.</span>
+            </div>
+            <input v-model="trackForm.title" id="title" name="title" type="text" placeholder="Enter track title"
+              class="input input-ghost bg-neutral w-full" />
+          </label>
+
+          <label class="form-control w-full">
+            <div class="label flex flex-col items-start">
+              <span class="label-text text-lg font-semibold">Price</span>
+            </div>
+            <label class="input input-ghost bg-neutral flex items-center">
+              <Icon icon="material-symbols:attach-money-rounded" width="24" height="24" />
+              <input v-model="trackForm.price" @input="numbersOnlyInput" @keydown="numbersOnlyInput" id="price"
+                name="price" type="number" placeholder="Enter track price" class="grow w-full" />
+            </label>
+          </label>
+
+          <label class="form-control w-full">
+            <div class="label flex flex-col items-start">
+              <span class="label-text text-lg font-semibold">Exclusive price(optional)</span>
+              <p class="label-text text-md">
+                <span class="text-error">*</span>
+                {{ ExclusivesAndStemsMessage }}
+              </p>
+            </div>
+            <label class="input input-ghost bg-neutral flex items-center">
+              <Icon icon="material-symbols:attach-money-rounded" class="text-warning" width="24" height="24" />
+              <input v-model="trackForm.exclusive_price" @input="numbersOnlyInput" @keydown="numbersOnlyInput"
+                id="exclusive_price" name="exclusive_price" type="number" placeholder="Enter track exclusive price"
+                class="grow w-full" />
+            </label>
+          </label>
+
+          <label class="form-control w-full">
+            <div class="label flex flex-col items-start">
+              <span class="label-text text-lg font-semibold">BPM</span>
+            </div>
+            <input v-model="trackForm.bpm" @input="numbersOnlyInput" @keydown="numbersOnlyInput" id="bpm" name="bpm"
+              type="number" placeholder="Enter track bpm" class="input input-ghost bg-neutral w-full" />
+          </label>
+        </section>
+
+        <div class="divider" />
+
+        <section class="flex flex-col gap-2 mt-4">
+          <h3 class="text-xl font-bold">Genres and Moods</h3>
+          <label class="form-control w-full">
+            <div class="label flex flex-col items-start">
+              <span class="label-text text-lg font-semibold">Genres</span>
+            </div>
+            <select v-model="selectedGenre" class="select select-ghost bg-neutral w-full" id="genres" name="genres">
+              <option disabled selected>Select a genre</option>
+              <option v-for="(genre, g) in genres" :key="g" :value="genre.pk" selected>
+                {{ genre.name }}
+              </option>
+            </select>
+          </label>
+
+          <label class="form-control w-full">
+            <div class="label flex flex-col items-start">
+              <span class="label-text text-lg font-semibold">Moods</span>
+            </div>
+            <select v-model="trackForm.mood" id="moods" name="moods" class="select select-ghost bg-neutral w-full">
+              <option disabled selected>Select a mood</option>
+              <option v-for="(mood, m) in moods" :key="m" :value="mood.pk" selected>
+                {{ mood.name }}
+              </option>
+            </select>
+          </label>
+        </section>
+
+        <div class="divider" />
+
+        <section class="flex flex-col gap-2 mt-4">
+          <h3 class="text-xl font-bold">Media files</h3>
+          <label class="form-control w-full">
+            <div class="label flex flex-col items-start">
+              <span class="label-text text-lg font-semibold">MP3</span>
+            </div>
+            <input @change="onMediaChange" type="file" class="file-input file-input-bordered w-full" accept=".mp3"
+              id="mp3" name="mp3" />
+          </label>
+
+          <label class="form-control w-full">
+            <div class="label flex flex-col items-start">
+              <span class="label-text text-lg font-semibold">WAV(optional)</span>
+            </div>
+            <input @change="onMediaChange" type="file" class="file-input file-input-bordered w-full" accept=".wav"
+              id="wav" name="wav" />
+          </label>
+
+          <div class="flex flex-col gap-3">
+            <div class="label flex flex-col items-start">
+              <span class="label-text text-lg font-semibold">Stems</span>
+              <p>Upload stems files in wav format.</p>
+              <p class="label-text text-md">
+                <span class="text-error">*</span>
+                {{ ExclusivesAndStemsMessage }}
+              </p>
+
+            </div>
+            <div v-for="(stem, s) in trackForm.stems" :key="s" class="flex flex-col gap-2">
+              <div class="flex gap-4">
+                <span class="label-text text-lg font-bold">Stem {{ s + 1 }}</span>
+                <div class="tooltip" data-tip="Remove stem">
+                  <Icon @click="removeStemHandler(s)" icon="solar:trash-bin-minimalistic-bold"
+                    class="cursor-pointer opacity-100 hover:opacity-80 active:opacity-60 text-error" width="24"
+                    height="24" />
+                </div>
+              </div>
+              <input v-model="stem.name" :id="`stem_${s}`" :name="`stem_${s}`" type="text" placeholder="Enter stem name"
+                class="input input-ghost bg-neutral w-full" />
+              <input @change="onStemChange(s, $event)" type="file" class="file-input file-input-bordered w-full"
+                accept=".wav" :id="`stem_file_${s}`" :name="`stem_file_${s}`" />
+            </div>
+          </div>
+
+          <a @click="addStemHandler()" class="btn btn-secondary rounded-[1rem] w-40">
+            <Icon icon="solar:add-square-bold" width="24" height="24" />
+            Add stem
+          </a>
+        </section>
+
+        <div class="divider" />
+
+        <section class="flex flex-col gap-4 mt-4">
+          <div class="flex flex-col gap-1">
+            <h3 class="text-xl font-bold">Collaborators</h3>
+            <p class="text-md">
+              <span class="text-error">*</span>
+              Split profits evenly amongst you and collaborators by adding thier Solana wallet address.
+            </p>
+          </div>
+          <div v-if="trackForm.collaborators.length > 0" class="flex flex-col gap-2">
+            <div v-for="(collab, c) in trackForm.collaborators" :key="c">
+              <label class="form-control w-full">
+                <div class="label flex flex-col items-start">
+                  <span class="label-text text-lg font-semibold">Collab {{ c + 1 }}</span>
+                </div>
+                <label class="input input-ghost bg-neutral flex items-center">
+                  <input v-model="collab.pubkey" :id="`collab_${c}`" :name="`collab_${c}`" type="text"
+                    placeholder="Enter wallet address" class="grow w-full" />
+                  <div class="tooltip" data-tip="Remove collab">
+                    <Icon @click.stop="removeCollabHandler(c)" icon="solar:trash-bin-minimalistic-bold"
+                      class="cursor-pointer opacity-100 hover:opacity-80 active:opacity-60 text-error" width="24"
+                      height="24" />
+                  </div>
+                </label>
+              </label>
+            </div>
+          </div>
+
+          <a @click="addCollabHandler()" class="btn btn-secondary rounded-[1rem] w-40">
+            <Icon icon="solar:user-plus-rounded-bold" width="24" height="24" />
+            Add collab
+          </a>
+        </section>
+      </form>
+
+      <div class="modal-action">
+        <form id="dialog-form" method="dialog" class="flex justify-end gap-2">
+          <button @click="closeModal()" class="btn btn-neutral">Close</button>
+          <button @click="submitHandler()" class="btn btn-primary" :disabled="!isTrackFormValid">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  </dialog>
+
 </template>
