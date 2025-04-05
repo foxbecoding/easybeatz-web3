e92a79f3151a34fdfd13e946fa3a1fe4be92b5f9
foxbecoding <foxbecoding@gmail.com>
2025-04-04 21:01:48 -0500

    adds serializer field class

--- api-venv/api/albums/serializers/track_page_serializer.py
+++ api-venv/api/albums/serializers/track_page_serializer.py
@@ -1,3 +1,11 @@
 from rest_framework import serializers
 from albums.models import Track
 
+class AlbumField(serializers.RelatedField):
+    def to_representation(self, value):
+        return {
+            "aid": value.aid,
+            "title": value.title,
+            "cover": value.cover.cover_url
+        }
+
