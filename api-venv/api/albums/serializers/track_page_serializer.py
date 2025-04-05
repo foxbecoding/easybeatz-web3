c14d73ad9f31f2f20ecbb6c20247e02a8e4e7289
foxbecoding <foxbecoding@gmail.com>
2025-04-04 21:02:12 -0500

    implements serializer

--- api-venv/api/albums/serializers/track_page_serializer.py
+++ api-venv/api/albums/serializers/track_page_serializer.py
@@ -25,3 +25,47 @@ class ExclusivePriceField(serializers.RelatedField):
     def to_representation(self, value):
         return value.value
 
+class TrackPageSerializer(serializers.ModelSerializer):
+    album = AlbumField(read_only=True)
+    display = DisplayField(read_only=True)
+    price = PriceField(read_only=True)
+    mood = MoodField(read_only=True)
+    exclusive_price = ExclusivePriceField(read_only=True)
+    genres = serializers.SerializerMethodField()
+    station = serializers.SerializerMethodField()
+    formatted_duration = serializers.SerializerMethodField()
+
+    def get_station(self, obj):
+        station = obj.album.station 
+
+        return {
+            "name": station.name,
+            "handle": station.handle,
+            "pubkey": station.user.pubkey,
+            "picture": station.picture.picture_url
+        }
+
+    def get_formatted_duration(self, obj):
+        return obj.formatted_duration
+
+    def get_genres(self, obj):
+        genres = [{ "name": genre.name, "slug": genre.slug } for genre in obj.genres.all()]
+        return genres[0]
+
+    class Meta:
+        model = Track
+        fields = [
+            "bpm",
+            "display",
+            "duration",
+            "exclusive_price",
+            "formatted_duration",
+            "genres",
+            "mood",
+            "order_no",
+            "price",
+            "tid",
+            "title",
+            "album",
+            "station",
+        ]
