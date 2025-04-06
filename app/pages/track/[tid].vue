cdeef6b63a15d33aaaee8a5b3bbe4e10edc872b5
foxbecoding <foxbecoding@gmail.com>
2025-04-05 19:40:01 -0500

    adds track request logic

--- app/pages/track/[tid].vue
+++ app/pages/track/[tid].vue
@@ -9,6 +9,27 @@ const authStore = useAuthStore();
 const isAuthenticated = computed(() => authStore.isAuthenticated)
 const img = useImage();
 
+// Track request logic
+const tid = ref(route.params.tid)
+const fetchPath = `${config.public.API_TRACK}/${route.params.tid}/track_page/`;
+
+const { data: cachedTrack } = useNuxtData<{ message: string; data: any }>(`track-${tid.value}`);
+const { data: fetchedTrack, error, status, refresh } = await useLazyFetch<{ message: string; data: any }>(fetchPath, {
+  server: false,
+  key: `track-${tid.value}`,
+  watch: [isAuthenticated],
+  onRequest({ request, options }) {
+    if (authStore.accessToken) {
+      options.headers.set('Authorization', `Bearer ${authStore.accessToken}`)
+    }
+  },
+  onResponseError({ request, response, options }) {
+    console.log(response);
+  }
+});
+
+const track = computed<Track>(() => fetchedTrack.value?.data as Track || cachedTrack.value?.data as Track);
+
 <template>
   <AppPageContainer>
   </AppPageContainer>
