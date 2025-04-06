f41231b65ad2ec9efa89ea99db47b8dee2261b11
foxbecoding <foxbecoding@gmail.com>
2025-04-05 19:39:09 -0500

    adds page imports

--- app/pages/track/[tid].vue
+++ app/pages/track/[tid].vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import { type Track } from "@/services/models/album";
+import { useAuthStore } from "@/store/auth";
+import type { NuxtLink } from "~/components";
+
 <template>
   <AppPageContainer>
   </AppPageContainer>
