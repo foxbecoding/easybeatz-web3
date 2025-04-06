f9bf621330dca39818a00a9210680b8114f13f4c
foxbecoding <foxbecoding@gmail.com>
2025-04-05 19:39:16 -0500

    adds composables

--- app/pages/track/[tid].vue
+++ app/pages/track/[tid].vue
@@ -3,6 +3,12 @@ import { type Track } from "@/services/models/album";
 import { useAuthStore } from "@/store/auth";
 import type { NuxtLink } from "~/components";
 
+const route = useRoute();
+const config = useRuntimeConfig();
+const authStore = useAuthStore();
+const isAuthenticated = computed(() => authStore.isAuthenticated)
+const img = useImage();
+
 <template>
   <AppPageContainer>
   </AppPageContainer>
