0db8aa228af5a19c6f32411de21b7879c2194e5c
foxbecoding <foxbecoding@gmail.com>
2025-04-08 09:16:55 -0500

    adds imports

--- app/pages/track/[tid].vue
+++ app/pages/track/[tid].vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { type Track } from "@/services/models/album";
 import { useAuthStore } from "@/store/auth";
+import { type TrackList, useMusicPlayerStore } from "@/store/musicPlayer";
 
 const route = useRoute();
 const config = useRuntimeConfig();
