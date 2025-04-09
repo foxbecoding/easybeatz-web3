c8c6b5e8558cfbfaec6d919da2839faabb6144b0
foxbecoding <foxbecoding@gmail.com>
2025-04-09 14:35:25 -0500

    creates viewset

--- api-venv/api/carts/views.py
+++ api-venv/api/carts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import status
+from core.mixins import ResponseMixin
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import action
+from .models import Cart, CartItem
 
-# Create your views here.
+class CartViewSet(viewsets.ViewSet, ResponseMixin):
+    def get_permissions(self):
+        permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
+
+    @action(detail=False, methods=['post'])
+    def add_cart_item(self, request):
+        pass
+
