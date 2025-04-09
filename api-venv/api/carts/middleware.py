3a435b01f289c67b3ce6e5f45b1e4794e1784236
foxbecoding <foxbecoding@gmail.com>
2025-04-09 13:36:20 -0500

    modifies class method

--- api-venv/api/carts/middleware.py
+++ api-venv/api/carts/middleware.py
@@ -5,8 +5,15 @@ class CartIdMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        new_cart_id = None
         if 'cart_id' not in request.COOKIES:
-            request.cart_id = str(uuid.uuid4())
+            new_cart_id = str(uuid.uuid4())
+            request.cart_id = new_cart_id
         else:
             request.cart_id = request.COOKIES['cart_id']
-        return self.get_response(request)
+
+        response = self.get_response(request)
+
+        if new_cart_id:
+            response.set_cookie('cart_id', new_cart_id, max_age=60*60*24*30)  # 30 days
+        return response
