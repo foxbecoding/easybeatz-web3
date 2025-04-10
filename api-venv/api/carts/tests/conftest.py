66f047b41f35219bce02d361cefac21b76bfca54
foxbecoding <foxbecoding@gmail.com>
2025-04-10 10:58:58 -0500

    adds fixture

--- api-venv/api/carts/tests/conftest.py
+++ api-venv/api/carts/tests/conftest.py
@@ -4,3 +4,7 @@ from users.tests.conftest import default_user
 from albums.tests.conftest import default_track, default_track_price, default_track_exclusive_price
 from ..models import *
 
+# @pytest.fixture
+# def default_cart(db, default_user):
+#     return Cart.objects.create(user=default_user, cart_id='test-cart-123')
+
