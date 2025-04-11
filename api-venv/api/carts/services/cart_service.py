cd3234b9a4b39f6030d50eea25aaba73099d205c
foxbecoding <foxbecoding@gmail.com>
2025-04-11 13:15:34 -0500

    refactors method

--- api-venv/api/carts/services/cart_service.py
+++ api-venv/api/carts/services/cart_service.py
@@ -5,7 +5,7 @@ from albums.enums import TrackPriceEnum
 from albums.models import TrackPrice, TrackExclusivePrice
 
 
-def add_item_to_cart(cart_id: str, tid: str, pricing_type: str):
+def add_item_to_cart(cart_id: str, tid: str, pricing_type: str, user=None):
     valid_type_model_map = {
         TrackPriceEnum.TRACK_PRICE.value: TrackPrice,
         TrackPriceEnum.TRACK_EXCLUSIVE_PRICE.value: TrackExclusivePrice,
@@ -14,9 +14,19 @@ def add_item_to_cart(cart_id: str, tid: str, pricing_type: str):
     if pricing_type not in valid_type_model_map:
         return False, "Invalid track pricing", None
 
-    cart_instance = Cart.objects.filter(cart_id=cart_id).first()
-    if not cart_instance:
-        return False, "Cart not found", None
+     # Determine the appropriate cart based on auth state
+    if user and user.is_authenticated:
+        cart_instance, created = Cart.objects.get_or_create(
+            user=user,
+            deleted__isnull=True,
+            defaults={"cart_id": cart_id}  # only used if new cart is created
+        )
+    else:
+        cart_instance, created = Cart.objects.get_or_create(
+            cart_id=cart_id,
+            user__isnull=True,
+            deleted__isnull=True
+        )
 
     track_instance = Track.objects.filter(tid=tid).first()
     if not track_instance:
