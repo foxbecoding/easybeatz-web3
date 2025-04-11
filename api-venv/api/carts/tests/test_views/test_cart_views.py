32806db4c0def5e257f4c4f85c633c827e462ff0
foxbecoding <foxbecoding@gmail.com>
2025-04-10 21:16:42 -0500

    adds imports

--- api-venv/api/carts/tests/test_views/test_cart_views.py
+++ api-venv/api/carts/tests/test_views/test_cart_views.py
@@ -3,7 +3,13 @@ from rest_framework.test import APIClient
 from rest_framework import status
 from django.urls import reverse
 from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
 from users.tests.conftest import default_user
+from genres.tests.conftest import default_genre
+from moods.tests.conftest import default_mood
+from stations.tests.conftest import default_station
+from albums.tests.conftest import default_album, default_track, default_track_price, default_track_exclusive_price
+from carts.models import *
 import logging
 
 logger = logging.getLogger("carts")
