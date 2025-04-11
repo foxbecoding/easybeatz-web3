326e7c7b35cb315545300ed9794212193c01c3c0
foxbecoding <foxbecoding@gmail.com>
2025-04-11 13:19:04 -0500

    modifies test case

--- api-venv/api/users/tests/test_services.py
+++ api-venv/api/users/tests/test_services.py
@@ -53,7 +53,11 @@ class TestWeb3LoginService:
         mock_msg_generator.return_value = "message"
         
         data = {"pubkey": "test_pubkey", "originalMessage": "message", "signedMessage": "signature"}
-        service = Web3LoginService(data)
+        
+        factory = APIRequestFactory()
+        request = factory.post("/auth/web3-login/", data, format='json')
+
+        service = Web3LoginService(data, request)
         
         response = service.run()
         
