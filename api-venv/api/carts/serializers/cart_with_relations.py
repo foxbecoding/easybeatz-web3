c798946b0c7b4ab1fece02c7a8f1fcc05f8f4583
foxbecoding <foxbecoding@gmail.com>
2025-04-13 15:10:17 -0500

    implements serializer class

--- api-venv/api/carts/serializers/cart_with_relations.py
+++ api-venv/api/carts/serializers/cart_with_relations.py
@@ -2,34 +2,72 @@ from rest_framework import serializers
 from ..models import Cart, CartItem
 from albums.enums import TrackPriceEnum
 
-        }
+class CartItemWithRelationsSerializer(serializers.ModelSerializer):
+    price_type = serializers.SerializerMethodField()
+    price = serializers.SerializerMethodField()
+    track = serializers.SerializerMethodField()
+    album = serializers.SerializerMethodField()
+    station = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CartItem
+        fields = ["price_type", "price", "track", "album", "station"]
+
+    def get_price_type(self, obj):
+        return obj.price_type
+
+    def get_price(self, obj):
+        if obj.price_type == TrackPriceEnum.TRACK_PRICE.value:
+            return obj.track.price.value if obj.track.price else None
+        elif obj.price_type == TrackPriceEnum.TRACK_EXCLUSIVE_PRICE.value:
+            return obj.track.exclusive_price.value if obj.track.exclusive_price else None
+        return None
+
+    def get_track(self, obj):
+        track = obj.track
         return {
-            "type": value.price_type,
-            "track": {
-                "tid": value.track.tid,
-                "title": value.track.title,
-                "duration": value.track.duration,
-                "formatted_duration": value.track.formatted_duration,
-                "display": value.track.display.display_url,
-                "order_no": value.track.order_no,
-                "price": price_map.get(value.price_type)
-            },
-            "album": {
-                "aid": value.track.album.aid,
-                "cover": value.track.album.cover.cover_url,
-            },
-            "station": {
-                "name": value.track.album.station.name,
-                "handle": value.track.album.station.handle,
-                "picture": value.track.album.station.picture_url,
-                "pubkey": value.track.album.station.user.pubkey
-            }
+            "tid": track.tid,
+            "title": track.title,
+            "duration": track.duration,
+            "formatted_duration": track.formatted_duration,
+            "display": track.display.display_url,
+            "order_no": track.order_no,
+        }
+
+    def get_album(self, obj):
+        album = obj.track.album
+        return {
+            "aid": album.aid,
+            "cover": album.cover.cover_url if album.cover else None
+        }
+
+    def get_station(self, obj):
+        station = obj.track.album.station
+        return {
+            "name": station.name,
+            "handle": station.handle,
+            "picture": station.picture.picture.url if station.picture else None,
+            "pubkey": station.user.pubkey
         }
 
 class CartWithRelationsSerializer(serializers.ModelSerializer):
-    items = ItemsField(read_only=True) 
-
+    items = CartItemWithRelationsSerializer(many=True, read_only=True)
+    # items = ItemsField(read_only=True, many=True) 
     class Meta:
         model = Cart
         fields = ["items"]
+
+
+    # items = serializers.SerializerMethodField()
+   
+    # def get_items(self, obj):
+    #     items = obj.items.all()
+    #     collection = []
+    #     for item in items:
+    #         price_map = {
+    #             TrackPriceEnum.TRACK_PRICE.value: item.track.price.value,
+    #             TrackPriceEnum.TRACK_EXCLUSIVE_PRICE.value: .track.exclusive_price.value,
+    #         }
+    #        collection.append() 
+    #     return collection
 
