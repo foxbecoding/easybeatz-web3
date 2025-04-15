9c19894c42b2af6c2ae4e34f394cbbb405f94f5e
foxbecoding <foxbecoding@gmail.com>
2025-04-15 10:12:29 -0500

    adds store prop

--- app/store/cart.ts
+++ app/store/cart.ts
@@ -3,6 +3,7 @@ import { type CartItem } from "@/services/models/cart";
 export const useCartStore = defineStore("use-cart-store", () => {
   const items = ref<CartItem[]>([])
   const cart_count = ref(0);
+  const cart_subtotal = ref(0);
 
   return {
     items
