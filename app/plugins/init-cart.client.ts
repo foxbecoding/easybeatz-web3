d8bced9ad94218e6604c23e18a132d1d8eedb9f4
foxbecoding <foxbecoding@gmail.com>
2025-04-15 10:11:01 -0500

    implements plugin logic

--- app/plugins/init-cart.client.ts
+++ app/plugins/init-cart.client.ts
@@ -1,3 +1,15 @@
 import { useCartStore } from "@/store/cart";
 import { CartResponse, getCart } from "@/services/models/cart";
 
+export default defineNuxtPlugin(async (nuxtApp) => {
+  const cartStore = useCartStore()
+
+  const { message, data } = await getCart();
+  const response_data = data as CartResponse;
+  if (!response_data.items) return;
+
+  if (response_data.items.length === 0) return;
+  cartStore.setCartItems(response_data.items);
+  cartStore.setCartCount(response_data.cart_count);
+  cartStore.setCartSubtotal(response_data.cart_subtotal);
+})
