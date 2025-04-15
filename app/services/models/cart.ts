0cbeb35dbf8578455cd931179df86d018dd01b3b
foxbecoding <foxbecoding@gmail.com>
2025-04-15 09:13:13 -0500

    implements interface

--- app/services/models/cart.ts
+++ app/services/models/cart.ts
@@ -1,5 +1,11 @@
 import { TrackPriceEnum } from "@/services/enums/track-price-enums";
 
+export interface CartResponse {
+    items: CartItem[];
+    cart_count: number;
+    cart_subtotal: number;
+};
+
 export const getCart = () => {
     const config = useRuntimeConfig();
     return submitRequest('GET', `${config.public.API_CART}/get-cart/`);
