4987882a57f3f17e1caa7180f4257ad121bf02bb
foxbecoding <foxbecoding@gmail.com>
2025-04-14 22:10:45 -0500

    fixes: test case error

--- api-venv/api/carts/tests/test_views/test_cart_views.py
+++ api-venv/api/carts/tests/test_views/test_cart_views.py
@@ -225,6 +225,6 @@ class TestCartViewSet:
         response = client.post(url, request_data )
 
         assert response.status_code == status.HTTP_400_BAD_REQUEST
-        assert response.data.get("message") == "Invalid track pricing"
+        assert response.data.get("message") == "Invalid track pricing type"
         assert response.data.get("data") is None 
 
