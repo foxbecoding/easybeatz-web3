3ee18718d35b5c43ffad20a6a7c9335d0f1c3e95
foxbecoding <foxbecoding@gmail.com>
2025-04-15 17:01:04 -0500

    adds computed button label

--- app/pages/track/[tid].vue
+++ app/pages/track/[tid].vue
@@ -247,7 +247,11 @@ const addCartItemHandler = async (tid: string, type: TrackPriceEnum.TRACK_PRICE 
             </ul>
             <div class="card-actions">
               <button @click="addCartItemHandler(tid.toString(), TrackPriceEnum.TRACK_EXCLUSIVE_PRICE)"
-                class="btn btn-warning btn-block rounded-[1rem] text-lg">Add to cart</button>
+                class="btn btn-warning btn-block rounded-[1rem] text-lg" :disabled="false">
+                {{ cartBtnLabelExclusive }}
+                <span v-if="isAddingCartItem[TrackPriceEnum.TRACK_EXCLUSIVE_PRICE]"
+                  class="loading loading-dots loading-md"></span>
+              </button>
             </div>
           </div>
         </div>
