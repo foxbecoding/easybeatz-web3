747d28715c001996d7031de3a62962671b61093b
foxbecoding <foxbecoding@gmail.com>
2025-04-19 21:28:53 -0500

    adds script logic

---
+++ app/pages/cart/components/CartItemList.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import CartItem from "./CartItem.vue"
+import { type CartItem as CartItemType } from "@/services/models/cart"
+
+defineProps<{ cartItems: CartItemType[] }>();
+
+</script>
+
