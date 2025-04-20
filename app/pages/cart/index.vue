f98c475c5b7c6fb6073a0637e7a688ff3d11aee2
foxbecoding <foxbecoding@gmail.com>
2025-04-19 21:28:32 -0500

    adds script logic

---
+++ app/pages/cart/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import CartItemList from "./components/CartItemList.vue";
+import { useCartStore } from "@/store/cart";
+
+const cartStore = useCartStore();
+const subtotal = computed(() => `Subtotal: $${cartStore.cart_subtotal}`)
+const isCartEmpty = computed(() => useCart().isCartEmpty);
+
+</script>
+
